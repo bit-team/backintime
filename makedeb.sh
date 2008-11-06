9c176d76c61a3b0a9e6a364a38d8754ebbe708c2
Oprea Dan <doprea@vbox>
2008-11-06 10:43:08 +0100

    Add all languages in one install/package

--- makedeb.sh
+++ makedeb.sh
@@ -15,22 +15,3 @@ dpkg --build deb/ $PKGNAME-${PKGVER}_$PKGARCH.deb
 
 rm -rf deb
 
-#translations
-./translate.sh
-
-for lang in `ls locale | cut -d/ -f2`; do
-	echo Make package for language: $lang
-
-	mkdir -p deb/DEBIAN
-
-	echo "Package: $PKGNAME-$lang" > deb/DEBIAN/control
-	echo "Depends: backintime" >> deb/DEBIAN/control
-	echo "Description: Back In Time locale for language '$lang'" >> deb/DEBIAN/control
-	cat control | grep -v Package | grep -v Depends | grep -v Description >> deb/DEBIAN/control
-
-	sh install_locale.sh $lang ./deb
-	dpkg --build deb/ $PKGNAME-$lang-${PKGVER}_$PKGARCH.deb
-
-	rm -rf deb
-done
-
