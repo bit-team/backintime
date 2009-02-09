e4c6a6b3082b0c8bc99e32f109f05bcda0824093
user <user@ubuntu>
2009-02-09 15:43:10 +0100

    Version 0.9.7beta1

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ gettext.textdomain( 'backintime' )
 
 class Config( configfile.ConfigFile ):
 	APP_NAME = 'Back In Time'
-	VERSION = '0.9.6'
+	VERSION = '0.9.7beta1'
 	COPYRIGHT = 'Copyright (c) 2008-2009 Oprea Dan'
 
 	NONE = 0
