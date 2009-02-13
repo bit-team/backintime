6304c8d36891b79a9a6cdfa348c98f33eca8e6d7
user <user@ubuntu>
2009-02-13 10:13:19 +0100

    0.9.7beta3

--- updateversion.sh
+++ updateversion.sh
@@ -4,42 +4,28 @@ VERSION=`cat VERSION`
 echo VERSION: $VERSION
 
 echo "Update 'control.common'"
-mv control.common control.tmp
-sed -e "s/^Version: .*$/Version: $VERSION/" control.tmp > control.common
-rm control.tmp
+sed -i -e "s/^Version: .*$/Version: $VERSION/" control.common
 
 echo "Update 'control.gnome'"
-mv control.gnome control.tmp
-sed -e "s/^Version: .*$/Version: $VERSION/" control.tmp | sed -e "s/backintime-common (= [^)]*)/backintime-common (= $VERSION)/" > control.gnome
-rm control.tmp
+sed -i -e "s/^Version: .*$/Version: $VERSION/" control.gnome
+sed -i -e "s/backintime-common (= [^)]*)/backintime-common (= $VERSION)/" control.gnome
 
 echo "Update 'control.kde4'"
-mv control.kde4 control.tmp
-sed -e "s/^Version: .*$/Version: $VERSION/" control.tmp | sed -e "s/backintime-common (= [^)]*)/backintime-common (= $VERSION)/" > control.kde4
-rm control.tmp
+sed -i -e "s/^Version: .*$/Version: $VERSION/" control.kde4
+sed -i -e "s/backintime-common (= [^)]*)/backintime-common (= $VERSION)/" control.kde4
 
 echo "Update 'config.py'"
-mv config.py config.py.tmp
-sed -e "s/^\tVERSION = '.*'$/\tVERSION = '$VERSION'/" config.py.tmp  > config.py
-rm config.py.tmp
+sed -i -e "s/^\tVERSION = '.*'$/\tVERSION = '$VERSION'/" config.py
 
 echo "Update man page"
 FILE=man/C/backintime.1
 gzip -d $FILE.gz
-mv $FILE $FILE.tmp
-sed -e "s/\.TH\(.*\)\"version\([^\"]*\)\"\(.*\)$/.TH\1\"version $VERSION\"\3/" $FILE.tmp > $FILE
-rm $FILE.tmp
+sed -i -e "s/\.TH\(.*\)\"version\([^\"]*\)\"\(.*\)$/.TH\1\"version $VERSION\"\3/" $FILE
 gzip --best $FILE
 
 echo "Update help .omf file"
-FILE=docbook/C/backintime-C.omf
-mv $FILE $FILE.tmp
-sed -e "s/^\([ \]*\)<version\([^0-9]*\)\([^\"]*\)\(.*\)$/\1<version\2$VERSION\4/" $FILE.tmp > $FILE
-rm $FILE.tmp
+sed -i -e "s/^\([ \]*\)<version\([^0-9]*\)\([^\"]*\)\(.*\)$/\1<version\2$VERSION\4/" docbook/C/backintime-C.omf
 
 echo "Update help docbook file"
-FILE=docbook/C/backintime.xml
-mv $FILE $FILE.tmp
-sed -e "s/^<!ENTITY appversion .*>$/<!ENTITY appversion \"$VERSION\">/" -e "s/^<!ENTITY manrevision .*>$/<!ENTITY manrevision \"$VERSION\">/" $FILE.tmp > $FILE
-rm $FILE.tmp
+sed -i -e "s/^<!ENTITY appversion .*>$/<!ENTITY appversion \"$VERSION\">/" -e "s/^<!ENTITY manrevision .*>$/<!ENTITY manrevision \"$VERSION\">/" docbook/C/backintime.xml
 
