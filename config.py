4feb9444d50c0cdc410af7a66d8daf47e80353ec
Dan <dan@xps1530>
2009-03-04 08:56:37 +0100

    Version 0.9.13beta2

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ gettext.textdomain( 'backintime' )
 
 class Config( configfile.ConfigFile ):
 	APP_NAME = 'Back In Time'
-	VERSION = '0.9.13beta1'
+	VERSION = '0.9.13beta2'
 	COPYRIGHT = 'Copyright (c) 2008-2009 Oprea Dan'
 
 	NONE = 0
