4e2f9600f9772b67592bad4a9559a253d5267846
Richard Bailey <rmjb@mail.com>
2009-10-14 21:35:38 -0400

    Switched to a more standard crontab syntax

--- common/config.py
+++ common/config.py
@@ -492,15 +492,15 @@ class Config( configfile.ConfigFileWithProfiles ):
 			elif self._10_MIN == min_backup_mode:
 				cron_line = 'echo "*/10 * * * * {cmd}"'
 			if self.HOUR == min_backup_mode:
-				cron_line = 'echo "@hourly {cmd}"'
+				cron_line = 'echo "0 * * * * {cmd}"'
 			elif self.DAY == min_backup_mode:
-				cron_line = 'echo "@daily {cmd}"'
+				cron_line = 'echo "0 0 * * * {cmd}"'
 			elif self.WEEK == min_backup_mode and self.MONTH == max_backup_mode: #for every-week and every-month use every-day
-				cron_line = 'echo "@daily {cmd}"'
+				cron_line = 'echo "0 0 * * * {cmd}"'
 			elif self.WEEK == min_backup_mode:
-				cron_line = 'echo "@weekly {cmd}"'
+				cron_line = 'echo "0 0 * * 0 {cmd}"'
 			elif self.MONTH == min_backup_mode:
-				cron_line = 'echo "@monthly {cmd}"'
+				cron_line = 'echo "0 0 1 * * {cmd}"'
 
 			if len( cron_line ) > 0:
 				cmd = "/usr/bin/backintime --profile \\\"%s\\\" --backup-job >/dev/null 2>&1" % profile_name
