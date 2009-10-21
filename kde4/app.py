5185d5012f16a959713a44ddb5575bca622c3c8f
Bart de Koning <bratdaking@gmail.com>
2009-10-21 15:30:08 +0200

    Profile fix in kde

--- kde4/app.py
+++ kde4/app.py
@@ -299,8 +299,9 @@ class MainWindow( KMainWindow ):
 
 		if not cfg.is_configured():
 			return
-
-		if not cfg.can_backup():
+		
+		profile_id = cfg.get_current_profile()
+		if not cfg.can_backup( profile_id ):
 			KMessageBox.error( self, QString.fromUtf8( _('Can\'t find snapshots folder.\nIf it is on a removable drive please plug it and then press OK') ) )
 
 		QObject.connect( self.list_files_view_model.dirLister(), SIGNAL('completed()'), self.on_dir_lister_completed )
