4c1ce7db101e4f6d5787c52542308d0430e30007
Dan <dan@dan-laptop>
2010-03-02 22:09:52 +0100

    dev

--- makedeb-src.sh
+++ makedeb-src.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-RELEASES="hardy intrepid jaunty karmic"
+#RELEASES="hardy intrepid jaunty karmic"
+RELEASES="karmic"
 
 for i in common gnome kde4; do
 	PKGNAME=`cat $i/debian_specific/control | grep "^Package:" | cut -d" " -f2`
@@ -19,9 +20,9 @@ for i in common gnome kde4; do
 	cd tmp/${PKGNAME}_$PKGVER
 
 	for release in $RELEASES; do
-		#debuild -i -us -uc -S
 		sed -e "1s/.*/$PKGNAME ($PKGVER~$release) $release; urgency=low/" -i debian/changelog
 		debuild -i -S
+		#debuild -i -us -uc -S
 	done
 
 	cd ..
