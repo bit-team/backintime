9049439510eea0a9163faa0511878266b8d2473a
user <user@ubuntu>
2010-03-04 12:23:37 +0100

    dev

--- makedeb-src.sh
+++ makedeb-src.sh
@@ -20,7 +20,14 @@ for i in common gnome kde4; do
 	cd tmp/${PKGNAME}_$PKGVER
 
 	for release in $RELEASES; do
-		sed -e "1s/.*/$PKGNAME ($PKGVER~$release) $release; urgency=low/" -i debian/changelog
+		#debian: control
+		cp ../../$i/debian_specific/control.source debian/control
+		sed -e "s/backintime-common (= [^)]*)/backintime-common (= $PKGVER~$release)/g" -i debian/control
+
+		#debian: changelog
+		cp ../../common/debian_specific/changelog debian
+		sed -e "s/\$BACKINTIME/backintime-$i/g" -e "s/\$VERSION/$PKGVER/g"  -e "s/\$RELEASE/$release/g" -i debian/changelog
+
 		debuild -i -S
 		#debuild -i -us -uc -S
 	done
