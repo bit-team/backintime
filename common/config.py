e49a0313986b7164395aa067d0f0f8ebbd3891bd
danleweb <dan@le-web.org>
2011-06-05 14:38:01 +0200

    Version: 1.0.8

--- common/config.py
+++ common/config.py
@@ -35,7 +35,7 @@ gettext.textdomain( 'backintime' )
 
 class Config( configfile.ConfigFileWithProfiles ):
 	APP_NAME = 'Back In Time'
-	VERSION = '1.0.7'
+	VERSION = '1.0.8'
 	COPYRIGHT = 'Copyright (c) 2008-2009 Oprea Dan, Bart de Koning, Richard Bailey'
 	CONFIG_VERSION = 5
 
