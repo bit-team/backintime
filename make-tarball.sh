d666181220c02ac5e14435746a03d667dd9c74e2
Germar <germar.reitze@gmail.com>
2014-08-20 23:34:58 +0200

    add top-level directory for tarball (fixed https://launchpad.net/bugs/1359076)

--- make-tarball.sh
+++ make-tarball.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 VER=`cat VERSION`
+CURRENT=$(pwd)
+NEW="backintime-$VER"
 
-tar cfz backintime-$VER.tar.gz AUTHORS CHANGES LICENSE TODO README TRANSLATIONS VERSION *.sh common qt4
+cd ..
+bzr branch $CURRENT $NEW
+tar cfz backintime-$VER.tar.gz ${NEW}/AUTHORS ${NEW}/CHANGES ${NEW}/LICENSE ${NEW}/TODO ${NEW}/README ${NEW}/TRANSLATIONS ${NEW}/VERSION ${NEW}/*.sh ${NEW}/common ${NEW}/qt4
 
