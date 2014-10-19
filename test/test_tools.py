ec9fe150034eebd9cf3c593c73da096d4cdbbbba
Dorian Burihabwa dorian.burihabwa@agil-it.fr <>
2014-10-19 14:54:15 +0200

    Pep8 formatting of tests scripts

--- test/test_tools.py
+++ test/test_tools.py
@@ -1,5 +1,6 @@
 import unittest
-import sys, os
+import os
+import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common/'))
 import tools
 import config
@@ -7,37 +8,42 @@ import configfile
 
 ##################################################
 #             How run that test ?                #
-#------------------------------------------------#
+# ---------------------------------------------- #
 # use py.test-3 (sudo apt-get install py.test-3) #
 #       py.test-3 -v test_tools.py               #
 ##################################################
 
 ################################################################
 #                         Output                               #
-#--------------------------------------------------------------#
+# ------------------------------------------------------------ #
 # If something is red so something going wrong with BackInTime #
 #         Otherwise all it's good ! you can commit !           #
 ################################################################
 
-''' All funtions test here come from tools.py '''
+
 class TestTools(unittest.TestCase):
+    ''' All funtions test here come from tools.py '''
 
     def test_read_file(self):
         ''' Test the function read_file '''
-        test_directory = os.path.dirname(os.path.abspath(__file__))
-        self.assertNotEquals(tools.read_file(os.path.join(test_directory, "test_tools.py")), None);
-        self.assertEquals(tools.read_file(os.path.join(test_directory, test_directory + "notExistedFile")), None);
+        test_tools_file = os.path.abspath(__file__)
+        test_directory = os.path.dirname(test_tools_file)
+        non_existing_file = os.path.join(test_directory, "notExistedFile")
+        self.assertNotEquals(tools.read_file(test_tools_file), None)
+        self.assertEquals(tools.read_file(non_existing_file), None)
 
     def test_read_file_lines(self):
         ''' Test the function read_file_lines '''
-        test_directory = os.path.dirname(os.path.abspath(__file__))
-        print("Test directory : " + test_directory)
-        self.assertNotEquals(tools.read_file(os.path.join(test_directory, "test_tools.py")), None);
-        self.assertEquals(tools.read_file(os.path.join(test_directory, "/notExistedFile")), None);
+        test_tools_file = os.path.abspath(__file__)
+        test_directory = os.path.dirname(test_tools_file)
+        non_existing_file = os.path.join(test_directory, "notExistedFile")
+        self.assertNotEquals(tools.read_file(test_tools_file), None)
+        self.assertEquals(tools.read_file(non_existing_file), None)
 
     def test_read_command_output(self):
         ''' Test the function read_command_output '''
-        self.assertEquals("Test, read command output", tools.read_command_output("echo 'Test, read command output'"));
+        ret_val = tools.read_command_output("echo 'Test, read command output'")
+        self.assertEquals("Test, read command output", ret_val)
 
     def test_check_command(self):
         ''' Test the function check_command '''
@@ -51,36 +57,40 @@ class TestTools(unittest.TestCase):
 
     def test_process_exists(self):
         ''' Test the function process_exists '''
-        self.assertTrue(tools.process_exists("init"));
-        self.assertFalse(tools.process_exists("notExistedProcess"));
+        self.assertTrue(tools.process_exists("init"))
+        self.assertFalse(tools.process_exists("notExistedProcess"))
 
     def test_load_env(self):
         ''' Test the function load_env '''
         dict = {}
         lines = []
         path_user = os.path.expanduser('~')
-        path_cron_env = os.path.join(path_user, ".local/share/backintime/cron_env")
+        path_cron_env = os.path.join(
+            path_user,
+            ".local/share/backintime/cron_env")
         try:
-            with open( path_cron_env, 'rt' ) as file:
+            with open(path_cron_env, 'rt') as file:
                 lines = file.readlines()
-        except :
+        except:
             pass
         for line in lines:
-            items = line.split( '=', 1)
-            if len( items ) == 2:
-                dict[ items[ 0 ] ] = items[ 1 ][ : -1 ]
-        cfg = config.Config(None);
+            items = line.split('=', 1)
+            if len(items) == 2:
+                dict[items[0]] = items[1][:-1]
+        cfg = config.Config(None)
         tools.load_env(cfg)
         for key in dict.keys():
             self.assertEquals(os.environ[key], dict[key])
 
     def test_prepare_path(self):
         ''' Test the function load_env '''
-        path_with_slash = "/test/path";
-        path_without_slash = "test/path";
-        self.assertEquals(tools.prepare_path(path_with_slash), path_with_slash);
-        self.assertEquals(tools.prepare_path(path_without_slash), path_with_slash);
-    
+        path_with_slash = "/test/path"
+        path_without_slash = "test/path"
+        self.assertEquals(tools.prepare_path(path_with_slash), path_with_slash)
+        self.assertEquals(
+            tools.prepare_path(path_without_slash),
+            path_with_slash)
+
     def test_is_process_alive(self):
         ''' Test the function is_process_alive '''
         self.assertTrue(tools.is_process_alive(0))
