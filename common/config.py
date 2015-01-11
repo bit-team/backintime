df6ba08c6ca456a85c617842bf58d8127cab7a54
Germar <germar.reitze@gmail.com>
2015-01-11 01:23:11 +0100

    version 1.1.0~beta03

--- common/config.py
+++ common/config.py
@@ -48,7 +48,7 @@ gettext.textdomain( 'backintime' )
 
 class Config( configfile.ConfigFileWithProfiles ):
     APP_NAME = 'Back In Time'
-    VERSION = '1.1.0~beta02'
+    VERSION = '1.1.0~beta03'
     COPYRIGHT = 'Copyright (C) 2008-2015 Oprea Dan, Bart de Koning, Richard Bailey, Germar Reitze'
     CONFIG_VERSION = 5
 
