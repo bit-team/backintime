4e87ec92efa4636468f10316db21979823968145
Germar Reitze <germar.reitze@gmail.com>
2015-10-13 22:37:29 +0200

    use README.md

--- make-tarball.sh
+++ make-tarball.sh
@@ -6,5 +6,5 @@ NEW="backintime-$VER"
 
 cd ..
 bzr branch $CURRENT $NEW
-tar cfz backintime-$VER.tar.gz ${NEW}/AUTHORS ${NEW}/CHANGES ${NEW}/LICENSE ${NEW}/TODO ${NEW}/README ${NEW}/TRANSLATIONS ${NEW}/VERSION ${NEW}/*.sh ${NEW}/common ${NEW}/qt4 ${NEW}/debian
+tar cfz backintime-$VER.tar.gz ${NEW}/AUTHORS ${NEW}/CHANGES ${NEW}/LICENSE ${NEW}/TODO ${NEW}/README.md ${NEW}/TRANSLATIONS ${NEW}/VERSION ${NEW}/*.sh ${NEW}/common ${NEW}/qt4 ${NEW}/debian
 
