1459e154df82f92a1c673badd3ecb07c79a5b311
Germar Reitze <germar.reitze@gmail.com>
2015-11-03 22:15:43 +0100

    use git instead on bzr; add fallback to cp

--- make-tarball.sh
+++ make-tarball.sh
@@ -5,6 +5,10 @@ CURRENT=$(pwd)
 NEW="backintime-$VER"
 
 cd ..
-bzr branch $CURRENT $NEW
+if [[ -n "$(which git)" ]] && [[ -x "$(which git)" ]]; then
+    git clone ${CURRENT} ${NEW}
+else
+    cp -aR ${CURRENT} ${NEW}
+fi
 tar cfz backintime-$VER.tar.gz ${NEW}/AUTHORS ${NEW}/CHANGES ${NEW}/LICENSE ${NEW}/TODO ${NEW}/README.md ${NEW}/TRANSLATIONS ${NEW}/VERSION ${NEW}/*.sh ${NEW}/common ${NEW}/qt4 ${NEW}/debian
 
