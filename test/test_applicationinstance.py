44a7b6efe2d6df3903ef838474bae81373f0bbf1
Germar Reitze <germar.reitze@gmail.com>
2015-11-27 03:34:35 +0100

    clean up code

--- test/test_applicationinstance.py
+++ test/test_applicationinstance.py
@@ -1,16 +1,16 @@
 # Back In Time
 # Copyright (C) 2008-2015 Oprea Dan, Bart de Koning, Richard Bailey, Germar Reitze
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public Licensealong
 # with this program; if not, write to the Free Software Foundation,Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -35,30 +35,29 @@ class TestApplicationInstance(unittest.TestCase):
         """
         #              GIVE               #
         # Creation of thread to get a pid
-        with open('temp.txt', 'wt') as output:
+        temp_file = 'temp.txt'
+        with open(temp_file, 'wt') as output:
             subproc = subprocess.Popen("top", stdout=output)
             pid = subproc.pid
         # Get the process name
         with open('/proc/%s/cmdline' % pid, 'r') as file:
             procname = file.read().strip('\n')
-            file.close()
 
         # create file with pid and process name
         file_name = "file_with_pid"
-        file_with_pid = open(file_name, "wt")
-        file_with_pid.write(str(pid) + "\n")
-        file_with_pid.write(procname)
-        file_with_pid.close()
+        with open(file_name, "wt") as file_with_pid:
+            file_with_pid.write(str(pid) + "\n")
+            file_with_pid.write(procname)
 
         #               WHEN             #
-        self.inst = ApplicationInstance(os.path.abspath(file_name), False)
-        result = self.inst.check()
+        inst = ApplicationInstance(os.path.abspath(file_name), False)
+        result = inst.check()
 
         #               THEN             #
         # Clean files and process
         subproc.kill()
-        os.remove("temp.txt")
-        os.remove("file_with_pid")
+        os.remove(temp_file)
+        os.remove(file_name)
         # Execute test
         self.assertFalse(result)
 
@@ -68,31 +67,30 @@ class TestApplicationInstance(unittest.TestCase):
         """
         #              GIVE               #
         # Creation of thread to get a pid
-        with open('temp.txt', 'wt') as output:
+        temp_file = 'temp.txt'
+        with open(temp_file, 'wt') as output:
             subproc = subprocess.Popen("top", stdout=output)
             pid = subproc.pid
         # Get the process name
         with open('/proc/%s/cmdline' % pid, 'r') as file:
             procname = file.read().strip('\n')
-            file.close()
 
         # create file with pid and process name
         file_name = "file_with_pid"
-        file_with_pid = open(file_name, "wt")
-        file_with_pid.write(str(pid) + "\n")
-        # Necessary, because os.kill can't kill Popen Process
-        file_with_pid.write(procname + "DELETE")
-        file_with_pid.close()
+        with open(file_name, "wt") as file_with_pid:
+            file_with_pid.write(str(pid) + "\n")
+            # Necessary, because os.kill can't kill Popen Process
+            file_with_pid.write(procname + "DELETE")
 
         #               WHEN             #
-        self.inst = ApplicationInstance(os.path.abspath(file_name), False)
-        result = self.inst.check()
+        inst = ApplicationInstance(os.path.abspath(file_name), False)
+        result = inst.check()
 
         #               THEN             #
         # Clean files and process
         subproc.kill()
-        os.remove("temp.txt")
-        os.remove("file_with_pid")
+        os.remove(temp_file)
+        os.remove(file_name)
 
         # Execute test
         self.assertTrue(result)
@@ -102,18 +100,17 @@ class TestApplicationInstance(unittest.TestCase):
         #              GIVE               #
         # create file with pid and process name
         file_name = "file_with_pid"
-        file_with_pid = open(file_name, "wt")
-        file_with_pid.write("987654321\n")
-        file_with_pid.write("FAKE_PROCNAME")
-        file_with_pid.close()
+        with open(file_name, "wt") as file_with_pid:
+            file_with_pid.write("987654321\n")
+            file_with_pid.write("FAKE_PROCNAME")
 
         #               WHEN             #
-        self.inst = ApplicationInstance(os.path.abspath(file_name), False)
-        result = self.inst.check()
+        inst = ApplicationInstance(os.path.abspath(file_name), False)
+        result = inst.check()
 
         #               THEN             #
         # Clean files and process
-        os.remove("file_with_pid")
+        os.remove(file_name)
 
         # Execute test
         self.assertTrue(result)
