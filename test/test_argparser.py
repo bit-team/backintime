03c21234d777d7690ea012e32cbb4c8672a21b52
Germar Reitze <germar.reitze@gmail.com>
2015-12-02 23:16:18 +0100

    test all permutations of args

--- test/test_argparser.py
+++ test/test_argparser.py
@@ -17,10 +17,24 @@
 import unittest
 import os
 import sys
+import itertools
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "common/"))
 import backintime
 
+def shuffleArgs(*args):
+    '''
+    Return every possible combination of arguments. Those arguments which need
+    to keep in line have to be inside a tuple.
+    '''
+    for i in itertools.permutations(args):
+        ret = []
+        for j in i:
+            if isinstance(j, (tuple, list)):
+                ret.extend(j)
+            else:
+                ret.append(j)
+        yield ret
 
 class TestArgParser(unittest.TestCase):
     def setUp(self):
@@ -38,6 +52,11 @@ class TestArgParser(unittest.TestCase):
         args = backintime.arg_parse(['--config', '/tmp/config'])
         self.assertIn('config', args)
         self.assertEqual(args.config, '/tmp/config')
+
+    def test_quiet(self):
+        args = backintime.arg_parse(['--quiet',])
+        self.assertIn('quiet', args)
+        self.assertTrue(args.quiet)
 
     def test_config_no_path(self):
         self.assertRaises(SystemExit, backintime.arg_parse, ['--config'])
@@ -62,8 +81,7 @@ class TestArgParser(unittest.TestCase):
         self.assertEqual(args.alias, 'backup')
 
     def test_cmd_backup_profile(self):
-        for argv in (['backup', '--profile', 'foo'],
-                     ['--profile', 'foo', 'backup']):
+        for argv in shuffleArgs('backup', ('--profile', 'foo')):
             with self.subTest(argv = argv):
                 #workaround for py.test3 2.5.1 doesn't support subTest
                 msg = 'argv = %s' %argv
@@ -91,9 +109,8 @@ class TestArgParser(unittest.TestCase):
         self.assertTrue(args.debug)
 
     def test_cmd_backup_multi_args(self):
-        for argv in (['--debug', 'backup', '--profile', 'foo', '--checksum'],
-                     ['backup', '--debug', '--checksum', '--profile', 'foo'],
-                     ['--checksum', '--profile', 'foo', '--debug', 'backup']):
+        for argv in shuffleArgs('--debug', 'backup', ('--profile', 'foo'), '--checksum',
+                                ('--config', 'bar')):
             with self.subTest(argv = argv):
                 #workaround for py.test3 2.5.1 doesn't support subTest
                 msg = 'argv = %s' %argv
@@ -106,6 +123,8 @@ class TestArgParser(unittest.TestCase):
                 self.assertTrue(args.debug, msg)
                 self.assertIn('checksum', args, msg)
                 self.assertTrue(args.checksum, msg)
+                self.assertIn('config', args, msg)
+                self.assertEqual(args.config, 'bar', msg)
 
     ############################################################################
     ###                               Restore                                ###
@@ -129,10 +148,9 @@ class TestArgParser(unittest.TestCase):
         self.assertEqual(args.SNAPSHOT_ID, '20151130-230501-984')
 
     def test_cmd_restore_what_where_snapshot_id_multi_args(self):
-        for argv in (['--debug', 'restore', '/home', '/tmp', '20151130-230501-984', '--checksum', '--profile-id', '2'],
-                     ['restore', '/home', '/tmp', '20151130-230501-984', '--debug', '--checksum', '--profile-id', '2'],
-                     ['--profile-id', '2', '--checksum', 'restore', '/home', '/tmp', '20151130-230501-984', '--debug'],
-                     ['--checksum', '--profile-id', '2', '--debug', 'restore', '/home', '/tmp', '20151130-230501-984']):
+        for argv in shuffleArgs('--debug', ('restore', '/home', '/tmp', '20151130-230501-984'),
+                                '--checksum', ('--profile-id', '2'), '--local-backup',
+                                '--delete', ('--config', 'foo')):
             with self.subTest(argv = argv):
                 #workaround for py.test3 2.5.1 doesn't support subTest
                 msg = 'argv = %s' %argv
@@ -151,6 +169,36 @@ class TestArgParser(unittest.TestCase):
                 self.assertEqual(args.WHERE, '/tmp', msg)
                 self.assertIn('SNAPSHOT_ID', args, msg)
                 self.assertEqual(args.SNAPSHOT_ID, '20151130-230501-984', msg)
+                self.assertIn('local_backup', args, msg)
+                self.assertTrue(args.local_backup, msg)
+                self.assertIn('delete', args, msg)
+                self.assertTrue(args.delete, msg)
+                self.assertIn('config', args, msg)
+                self.assertEqual(args.config, 'foo', msg)
+
+    def test_cmd_restore_multi_args(self):
+        for argv in shuffleArgs(('--profile-id', '2'), '--debug', 'restore', '--checksum',
+                                '--local-backup',
+                                '--delete', ('--config', 'foo')):
+            with self.subTest(argv = argv):
+                #workaround for py.test3 2.5.1 doesn't support subTest
+                msg = 'argv = %s' %argv
+                print(msg)
+                args = backintime.arg_parse(argv)
+                self.assertIn('debug', args, msg)
+                self.assertTrue(args.debug, msg)
+                self.assertIn('checksum', args, msg)
+                self.assertTrue(args.checksum, msg)
+                self.assertIn('profile_id', args, msg)
+                self.assertEqual(args.profile_id, 2, msg)
+                self.assertIn('command', args, msg)
+                self.assertEqual(args.command, 'restore', msg)
+                self.assertIn('local_backup', args, msg)
+                self.assertTrue(args.local_backup, msg)
+                self.assertIn('delete', args, msg)
+                self.assertTrue(args.delete, msg)
+                self.assertIn('config', args, msg)
+                self.assertEqual(args.config, 'foo', msg)
 
     def test_cmd_restore_snapshot_id_index(self):
         args = backintime.arg_parse(['restore', '/home', '/tmp', '1'])
@@ -161,3 +209,16 @@ class TestArgParser(unittest.TestCase):
         args = backintime.arg_parse(['restore', '/home', '', '20151130-230501-984'])
         self.assertIn('WHERE', args)
         self.assertEqual(args.WHERE, '')
+
+    def test_cmd_restore_where_space_in_path(self):
+        args = backintime.arg_parse(['restore', '/home', '/tmp/foo bar/baz', '20151130-230501-984'])
+        self.assertIn('WHERE', args)
+        self.assertEqual(args.WHERE, '/tmp/foo bar/baz')
+
+    def test_cmd_restore_what_space_in_path(self):
+        args = backintime.arg_parse(['restore', '/home/foo bar/baz', '/tmp', '20151130-230501-984'])
+        self.assertIn('WHAT', args)
+        self.assertEqual(args.WHAT, '/home/foo bar/baz')
+
+    def test_cmd_restore_local_backup_and_no_local_backup(self):
+        self.assertRaises(SystemExit, backintime.arg_parse, ('restore', '--local-backup', '--no-local-backup'))
