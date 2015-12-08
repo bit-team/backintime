3d157ad10ce76df5f0a6559dd42cf8d78973fa4f
Germar Reitze <germar.reitze@gmail.com>
2015-12-08 22:06:25 +0100

    clean up

--- makedeb.sh
+++ makedeb.sh
@@ -3,16 +3,5 @@
 PKGNAME=backintime
 PKGVER=$(cat VERSION)
 ARCH=all
-TMP=$(mktemp -d)
-CURRENT=$(pwd)
-
-DST=${TMP}/${PKGNAME}-${PKGVER}/
-mkdir ${DST}
-cp -aR ${CURRENT}/* ${DST}
-cd ${DST}
 
 dpkg-buildpackage -us -uc
-
-cd ..
-cp *.deb ${CURRENT}
-#rm -rf ${TMP}
