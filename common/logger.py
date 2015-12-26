671652ce52f1acdbd4d1f8ba76c7c41beb5392fb
Germar Reitze <germar.reitze@gmail.com>
2015-12-26 17:34:30 +0100

    add 'deprecated' logger

--- common/logger.py
+++ common/logger.py
@@ -66,6 +66,28 @@ def debug(msg, parent = None, traceDepth = 0):
         for line in tools.wrap_line(msg):
             syslog.syslog(syslog.LOG_DEBUG, 'DEBUG: %s' %line)
 
+def deprecated(parent = None):
+    frame = sys._getframe(1)
+    fdir, fname = os.path.split(frame.f_code.co_filename)
+    fmodule = os.path.basename(fdir)
+    line = frame.f_lineno
+    if parent:
+        fclass = '%s.' %parent.__class__.__name__
+    else:
+        fclass = ''
+    func = frame.f_code.co_name
+
+    frameCaller = sys._getframe(2)
+    fdirCaller, fnameCaller = os.path.split(frameCaller.f_code.co_filename)
+    fmoduleCaller = os.path.basename(fdirCaller)
+    lineCaller = frameCaller.f_lineno
+
+    msg = '%s/%s:%s %s%s called from ' %(fmodule, fname, line, fclass, func)
+    msgCaller = '%s/%s:%s' %(fmoduleCaller, fnameCaller, lineCaller)
+
+    print('%sDEPRECATED%s: %s%s%s%s' %(bcolors.WARNING, bcolors.ENDC, msg, bcolors.OKBLUE, msgCaller, bcolors.ENDC), file=sys.stderr)
+    syslog.syslog(syslog.LOG_WARNING, 'DEPRECATED: %s%s' %(msg, msgCaller))
+
 def _debugHeader(parent, traceDepth):
     frame = sys._getframe(2 + traceDepth)
     fdir, fname = os.path.split(frame.f_code.co_filename)
