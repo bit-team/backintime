c450150670f797484bd05cdd9346032167ae2f0c
Germar Reitze <germar.reitze@gmail.com>
2016-03-21 02:49:02 +0100

    change theme

--- common/doc-dev/conf.py
+++ common/doc-dev/conf.py
@@ -56,6 +56,7 @@ master_doc = 'index'
 # General information about the project.
 project = 'BackInTime'
 copyright = '2016, Germar Reitze'
+author = 'Germar Reitze'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -116,7 +117,7 @@ napoleon_include_private_with_doc = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -194,7 +195,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'BackInTimeDoc'
+htmlhelp_basename = 'BackInTimeDevDoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -214,7 +215,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'BackInTime.tex', 'BackInTime Documentation',
+  ('index', 'BackInTime.tex', 'Back In Time Development Documentation',
    'Germar Reitze', 'manual'),
 ]
 
@@ -244,7 +245,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'backintime', 'BackInTime Documentation',
+    ('index', 'backintime', 'Back In Time Development Documentation',
      ['Germar Reitze'], 1)
 ]
 
@@ -258,7 +259,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'BackInTime', 'BackInTime Documentation',
+  ('index', 'BackInTime', 'Back In Time Development Documentation',
    'Germar Reitze', 'BackInTime', 'One line description of project.',
    'Miscellaneous'),
 ]
