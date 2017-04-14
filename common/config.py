e490e504e05a88df130a12a5cea3730071756bb8
Germar Reitze <germar.reitze@gmail.com>
2017-04-14 17:51:29 +0200

    integrate gocrypt into Settings

--- common/config.py
+++ common/config.py
@@ -43,6 +43,7 @@ import configfile
 import logger
 import sshtools
 import encfstools
+import gocryptfstools
 import password
 import pluginmanager
 import schedule
@@ -272,7 +273,7 @@ class Config(configfile.ConfigFileWithProfiles):
                         sshtools.SSH, _('SSH'), _('SSH private key'), False),
                     'local_encfs': (
                         encfstools.EncFS_mount,
-                        _('Local encrypted'),
+                        _('Local encrypted') + ' (EncFS)',
                         _('Encryption'),
                         False
                     ),
@@ -281,7 +282,13 @@ class Config(configfile.ConfigFileWithProfiles):
                         _('SSH encrypted'),
                         _('SSH private key'),
                         _('Encryption')
-                    )
+                    ),
+                    'local_gocryptfs':(
+                        gocryptfstools.GoCryptFS_mount,
+                        _('Local encrypted') + ' (gocryptfs)',
+                        _('Encryption'),
+                        False
+                    ),
         }
 
         self.SSH_CIPHERS = {
@@ -693,6 +700,14 @@ class Config(configfile.ConfigFileWithProfiles):
 
     def setLocalEncfsPath(self, value, profile_id = None):
         self.setProfileStrValue('snapshots.local_encfs.path', value, profile_id)
+
+    # gocryptfs
+    def localGocryptfsPath(self, profile_id = None):
+        #?Where to save snapshots in mode 'local_gocryptfs'.;absolute path
+        return self.profileStrValue('snapshots.local_gocryptfs.path', '', profile_id)
+
+    def setLocalGocryptfsPath(self, value, profile_id = None):
+        self.setProfileStrValue('snapshots.local_gocryptfs.path', value, profile_id)
 
     def passwordSave(self, profile_id = None, mode = None):
         if mode is None:
