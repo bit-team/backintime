20bd6b34169fe7355d50e25be0049a4523f7f2ce
buhtz <c.buhtz@posteo.jp>
2022-09-16 12:00:20 +0200

    Last PEP8

--- common/sshtools.py
+++ common/sshtools.py
@@ -765,30 +765,37 @@ class SSH(MountControl):
                 f.write('foo')
 
             # check rsync
-            rsync1 =  tools.rsyncPrefix(self.config, no_perms = False, progress = False)
+            rsync1 =  tools.rsyncPrefix(
+                self.config, no_perms=False, progress=False)
             rsync1.append(tmp_file)
-            rsync1.append('%s@%s:"%s"/' %(self.user,
-                                        tools.escapeIPv6Address(self.host),
-                                        remote_tmp_dir_1))
+            rsync1.append('%s@%s:"%s"/' % (
+                self.user,
+                tools.escapeIPv6Address(self.host),
+                remote_tmp_dir_1))
 
             # check remote rsync hard-link support
-            rsync2 =  tools.rsyncPrefix(self.config, no_perms = False, progress = False)
-            rsync2.append('--link-dest=../%s' %os.path.basename(remote_tmp_dir_1))
+            rsync2 =  tools.rsyncPrefix(
+                self.config, no_perms=False, progress=False)
+            rsync2.append(
+                '--link-dest=../%s' % os.path.basename(remote_tmp_dir_1))
             rsync2.append(tmp_file)
-            rsync2.append('%s@%s:"%s"/' %(self.user,
-                                        tools.escapeIPv6Address(self.host),
-                                        remote_tmp_dir_2))
+            rsync2.append('%s@%s:"%s"/' % (
+                self.user,
+                tools.escapeIPv6Address(self.host),
+                remote_tmp_dir_2))
 
             for cmd in (rsync1, rsync2):
                 logger.debug('Check rsync command: %s' %cmd, self)
 
                 proc = subprocess.Popen(cmd,
-                                        stdout = subprocess.PIPE,
-                                        stderr = subprocess.PIPE,
-                                        universal_newlines = True)
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE,
+                                        universal_newlines=True)
                 out, err = proc.communicate()
+
                 if err or proc.returncode:
                     logger.debug('rsync command returned error: %s' %err, self)
+
                     raise MountException(_('Remote host %(host)s doesn\'t support \'%(command)s\':\n'
                                             '%(err)s\nLook at \'man backintime\' for further instructions')
                                             % {'host' : self.host, 'command' : cmd, 'err' : err})
@@ -834,9 +841,11 @@ class SSH(MountControl):
             cmd  = 'echo \"screen -d -m bash -c ...\"; screen -d -m bash -c \"true\" >/dev/null; err_screen=$?; '
             cmd += 'test $err_screen -ne 0 && cleanup $err_screen; '
             tail.append(cmd)
+
             cmd  = 'echo \"(flock -x 9) 9>smr.lock\"; bash -c \"(flock -x 9) 9>smr.lock\" >/dev/null; err_flock=$?; '
             cmd += 'test $err_flock -ne 0 && cleanup $err_flock; '
             tail.append(cmd)
+
             cmd  = 'echo \"rmdir \$(mktemp -d)\"; tmp3=$(mktemp -d); test -z "$tmp3" && cleanup 1; rmdir $tmp3 >/dev/null; err_rmdir=$?; '
             cmd += 'test $err_rmdir -ne 0 && cleanup $err_rmdir; '
             tail.append(cmd)
@@ -846,25 +855,27 @@ class SSH(MountControl):
         tail.append(cmd)
 
         maxLength = self.config.sshMaxArgLength(self.profile_id)
-        additionalChars = len('echo ""') + len(self.config.sshPrefixCmd(self.profile_id, cmd_type = str))
+        additionalChars = len('echo ""') \
+            + len(self.config.sshPrefixCmd(self.profile_id, cmd_type = str))
 
         output = ''
         err = ''
         returncode = 0
 
         for cmd in tools.splitCommands(tail,
-                                       head = head,
-                                       maxLength = maxLength - additionalChars):
+                                       head=head,
+                                       maxLength=maxLength-additionalChars):
             if cmd.endswith('; '):
                 cmd += 'echo ""'
 
-            c = self.config.sshCommand(cmd = [cmd],
-                                        custom_args = ['-p', str(self.port), self.user_host],
-                                        port = False,
-                                        user_host = False,
-                                        nice = False,
-                                        ionice = False,
-                                        profile_id = self.profile_id)
+            c = self.config.sshCommand(
+                cmd=[cmd],
+                custom_args=['-p', str(self.port), self.user_host],
+                port=False,
+                user_host=False,
+                nice=False,
+                ionice=False,
+                profile_id=self.profile_id)
 
             try:
                 logger.debug('Call command: %s' %' '.join(c), self)
@@ -875,19 +886,23 @@ class SSH(MountControl):
                 ret = proc.communicate()
 
             except OSError as e:
+
                 # Argument list too long
                 if e.errno == 7:
-                    logger.debug('Argument list too log (Python exception)', self)
+                    logger.debug(
+                        'Argument list too log (Python exception)', self)
+
                     return maxArg()
+
                 else:
                     raise
 
-            logger.debug('Command stdout: %s' %ret[0], self)
-            logger.debug('Command stderr: %s' %ret[1], self)
-            logger.debug('Command returncode: %s' %proc.returncode, self)
+            logger.debug('Command stdout: %s' % ret[0], self)
+            logger.debug('Command stderr: %s' % ret[1], self)
+            logger.debug('Command returncode: %s' % proc.returncode, self)
 
             output += ret[0].strip('\n') + '\n'
-            err    += ret[1].strip('\n') + '\n'
+            err += ret[1].strip('\n') + '\n'
             returncode += proc.returncode
 
             if proc.returncode:
@@ -899,6 +914,7 @@ class SSH(MountControl):
 
             if output_split and not output_split[-1]:
                 output_split = output_split[:-1]
+
             else:
                 break
 
@@ -907,16 +923,26 @@ class SSH(MountControl):
 
         if returncode or not output_split[-1].startswith('done'):
 
-            for command in ('rm', 'nice', 'ionice', 'nocache', 'screen', '(flock'):
+            for command in ('rm', 'nice', 'ionice',
+                            'nocache', 'screen', '(flock'):
 
                 if output_split[-1].startswith(command):
-                    raise MountException(_('Remote host %(host)s doesn\'t support \'%(command)s\':\n'
-                                            '%(err)s\nLook at \'man backintime\' for further instructions')
-                                            % {'host' : self.host, 'command' : output_split[-1], 'err' : err})
+                    raise MountException(
+                        _('Remote host %(host)s doesn\'t support'
+                          ' \'%(command)s\':\n%(err)s\nLook at \'man '
+                          'backintime\' for further instructions')
+                        % {
+                            'host': self.host,
+                            'command': output_split[-1],
+                            'err': err
+                        }
+                    )
 
-            raise MountException(_('Check commands on host %(host)s returned unknown error:\n'
-                                    '%(err)s\nLook at \'man backintime\' for further instructions')
-                                    % {'host' : self.host, 'err' : err})
+            raise MountException(
+                _('Check commands on host %(host)s returned unknown error:\n'
+                  '%(err)s\nLook at \'man backintime\' for further '
+                  'instructions')
+                  % {'host': self.host, 'err': err})
 
         inodes = []
 
@@ -931,7 +957,9 @@ class SSH(MountControl):
         logger.debug('remote inodes: ' + ' | '.join(inodes), self)
 
         if len(inodes) == 2 and inodes[0] != inodes[1]:
-            raise MountException(_('Remote host %s doesn\'t support hardlinks') % self.host)
+            raise MountException(
+                _('Remote host %s doesn\'t support hardlinks') % self.host)
+
 
     def randomId(self, size=6, chars=string.ascii_uppercase + string.digits):
         """
