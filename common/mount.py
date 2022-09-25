d57616947c2b0b3656de5b5eb80f1fb8f01a2dc9
buhtzz <c.buhtz@posteo.jp>
2022-09-25 08:46:56 +0200

    Update mount.py

--- common/mount.py
+++ common/mount.py
@@ -14,7 +14,7 @@
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-"""Modul docstring
+"""High-level mount API for mounting, umounting, checks etc.
 """
 
 import os
