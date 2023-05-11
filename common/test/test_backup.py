1c08d393f5a438595313c4c9c3001b86bf86fa6f
buhtz <c.buhtz@posteo.jp>
2023-05-11 23:27:23 +0200

    fix: asyncbackup tests

--- common/test/test_backup.py
+++ common/test/test_backup.py
@@ -18,10 +18,9 @@
 import os
 import sys
 import unittest
-from unittest.mock import patch
-from datetime import date, datetime
+from unittest.mock import patch, call
+from datetime import datetime
 from test import generic
-
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import backintime
@@ -34,8 +33,9 @@ from pluginmanager import PluginManager
 from mount import Mount
 from exceptions import MountException
 
+
+@patch('time.sleep')  # speed up unittest
 class TestBackup(generic.SnapshotsTestCase):
-    @patch('time.sleep') # speed up unittest
     @patch('snapshots.Snapshots.takeSnapshot')
     def test_backup(self, takeSnapshot, sleep):
         takeSnapshot.return_value = [True, False]
@@ -46,7 +46,6 @@ class TestBackup(generic.SnapshotsTestCase):
         self.assertIsInstance(takeSnapshot.call_args[0][1], datetime)
         self.assertIsInstance(takeSnapshot.call_args[0][2], list)
 
-    @patch('time.sleep') # speed up unittest
     @patch('subprocess.Popen', autospec=True)
     def test_backup_async(self, Popen_mock, sleep):
         """:py:func:`backintime.takeSnapshotAsync`:
@@ -62,23 +61,19 @@ class TestBackup(generic.SnapshotsTestCase):
         # The value will be reset in the setup() of the test.
         logger.DEBUG = False
 
-        self.assertIsNone(backintime.takeSnapshotAsync(self.cfg, checksum=False))
+        self.assertIsNone(
+            backintime.takeSnapshotAsync(self.cfg, checksum=False))
 
-        self.assertEqual(Popen_mock.call_count, 1)
+        expected_call = [
+            'backintime',
+            '--config',
+            self.cfgFile,
+            '--share-path',
+            self.cfg.DATA_FOLDER_ROOT,
+            'backup'
+        ]
+        Popen_mock.assert_called_once_with(expected_call, env=os.environ)
 
-        print("call_args: {}".format(Popen_mock.call_args))
-        print("call_args.__dict__: {}".format(Popen_mock.call_args.__dict__))
-        print("call_args.args[0]: " + "|".join(Popen_mock.call_args.args[0]))
-
-        # args_count = Popen_mock.call_args[0].count()
-        self.assertEqual(Popen_mock.call_args.args[0][0], "backintime")
-        self.assertEqual(Popen_mock.call_args.args[0][1], "--config")
-        self.assertEqual(Popen_mock.call_args.args[0][2], self.cfgFile)
-        self.assertEqual(Popen_mock.call_args.args[0][3], "--share-path")
-        self.assertEqual(Popen_mock.call_args.args[0][4], self.cfg.DATA_FOLDER_ROOT)
-        self.assertEqual(Popen_mock.call_args.args[0][5], "backup")
-
-    @patch('time.sleep') # speed up unittest
     @patch('subprocess.Popen', autospec=True)
     def test_backup_async_with_checksum(self, Popen_mock, sleep):
         """:py:func:`backintime.takeSnapshotAsync`:
@@ -94,22 +89,21 @@ class TestBackup(generic.SnapshotsTestCase):
         # The value will be reset in the setup() of the test.
         logger.DEBUG = False
 
-        self.assertIsNone(backintime.takeSnapshotAsync(self.cfg, checksum=True))
+        self.assertIsNone(
+            backintime.takeSnapshotAsync(self.cfg, checksum=True))
 
-        self.assertEqual(Popen_mock.call_count, 1)
+        expected_call = [
+            'backintime',
+            '--config',
+            self.cfgFile,
+            '--share-path',
+            self.cfg.DATA_FOLDER_ROOT,
+            '--checksum',
+            'backup'
+        ]
+        Popen_mock.assert_called_once_with(
+            expected_call, env=os.environ)
 
-        print(Popen_mock.call_args.args[0])
-
-        # args_count = Popen_mock.call_args[0].count()
-        self.assertEqual(Popen_mock.call_args.args[0][0], "backintime")
-        self.assertEqual(Popen_mock.call_args.args[0][1], "--config")
-        self.assertEqual(Popen_mock.call_args.args[0][2], self.cfgFile)
-        self.assertEqual(Popen_mock.call_args.args[0][3], "--share-path")
-        self.assertEqual(Popen_mock.call_args.args[0][4], self.cfg.DATA_FOLDER_ROOT)
-        self.assertEqual(Popen_mock.call_args.args[0][5], "--checksum")
-        self.assertEqual(Popen_mock.call_args.args[0][6], "backup")
-
-    @patch('time.sleep') # speed up unittest
     @patch('subprocess.Popen', autospec=True)
     def test_backup_async_profile_2(self, Popen_mock, sleep):
         """:py:func:`backintime.takeSnapshotAsync`:
@@ -126,27 +120,26 @@ class TestBackup(generic.SnapshotsTestCase):
         logger.DEBUG = False
 
         # Create and use a (new) profile (not the default '1')
-        new_rofile_id = self.cfg.addProfile("Profile #2")
-        self.cfg.setCurrentProfile(new_rofile_id)
+        new_profile_id = self.cfg.addProfile("Profile #2")
+        self.cfg.setCurrentProfile(new_profile_id)
 
-        self.assertIsNone(backintime.takeSnapshotAsync(self.cfg, checksum=False))
+        self.assertIsNone(
+            backintime.takeSnapshotAsync(self.cfg, checksum=False))
 
-        self.assertEqual(Popen_mock.call_count, 1)
+        expected_call = [
+            'backintime',
+            '--profile-id',
+            new_profile_id,
+            '--config',
+            self.cfgFile,
+            '--share-path',
+            self.cfg.DATA_FOLDER_ROOT,
+            'backup'
+        ]
 
-        print(Popen_mock.call_args.args[0])
+        Popen_mock.assert_called_once_with(
+            expected_call, env=os.environ)
 
-        # args_count = Popen_mock.call_args[0].count()
-        self.assertEqual(Popen_mock.call_args.args[0][0], "backintime")
-        self.assertEqual(Popen_mock.call_args.args[0][1], "--profile-id")
-        self.assertEqual(Popen_mock.call_args.args[0][2], new_rofile_id)
-        self.assertEqual(Popen_mock.call_args.args[0][3], "--config")
-        self.assertEqual(Popen_mock.call_args.args[0][4], self.cfgFile)
-        self.assertEqual(Popen_mock.call_args.args[0][5], "--share-path")
-        self.assertEqual(Popen_mock.call_args.args[0][6], self.cfg.DATA_FOLDER_ROOT)
-        self.assertEqual(Popen_mock.call_args.args[0][7], "backup")
-
-
-    @patch('time.sleep') # speed up unittest
     @patch('snapshots.Snapshots.takeSnapshot')
     def test_no_changes(self, takeSnapshot, sleep):
         takeSnapshot.return_value = [False, False]
@@ -158,13 +151,11 @@ class TestBackup(generic.SnapshotsTestCase):
         self.assertFalse(newSnapshot.exists())
         self.assertFalse(sid.exists())
 
-    @patch('time.sleep') # speed up unittest
     @patch('snapshots.Snapshots.takeSnapshot')
     def test_with_errors(self, takeSnapshot, sleep):
         takeSnapshot.return_value = [False, True]
         self.assertIs(self.sn.backup(), True)
 
-    @patch('time.sleep') # speed up unittest
     @patch('tools.onBattery')
     @patch('snapshots.Snapshots.takeSnapshot')
     def test_no_backup_on_battery(self, takeSnapshot, onBattery, sleep):
@@ -173,44 +164,42 @@ class TestBackup(generic.SnapshotsTestCase):
 
         # run on battery
         onBattery.return_value = True
-        self.assertFalse(self.sn.backup(force = False))
+        self.assertFalse(self.sn.backup(force=False))
         self.assertFalse(takeSnapshot.called)
 
         # force run
-        self.assertFalse(self.sn.backup(force = True))
+        self.assertFalse(self.sn.backup(force=True))
         self.assertTrue(takeSnapshot.called)
         takeSnapshot.reset_mock()
 
         # ignore Battery
         self.cfg.setNoSnapshotOnBattery(False)
-        self.assertFalse(self.sn.backup(force = False))
+        self.assertFalse(self.sn.backup(force=False))
         self.assertTrue(takeSnapshot.called)
 
-    @patch('time.sleep') # speed up unittest
     @patch('snapshots.Snapshots.takeSnapshot')
     def test_scheduled(self, takeSnapshot, sleep):
         # first run without timestamp
         takeSnapshot.return_value = [True, False]
-        self.assertFalse(self.sn.backup(force = False))
+        self.assertFalse(self.sn.backup(force=False))
         self.assertTrue(takeSnapshot.called)
         takeSnapshot.reset_mock()
 
         # second run doesn't use an anacron-like schedule
         tools.writeTimeStamp(self.cfg.anacronSpoolFile())
-        self.assertFalse(self.sn.backup(force = False))
+        self.assertFalse(self.sn.backup(force=False))
         self.assertTrue(takeSnapshot.called)
         takeSnapshot.reset_mock()
 
         # third run uses anacron-like schedule so it should not run
         self.cfg.setScheduleMode(self.cfg.REPEATEDLY)
-        self.assertFalse(self.sn.backup(force = False))
+        self.assertFalse(self.sn.backup(force=False))
         self.assertFalse(takeSnapshot.called)
 
         # finally force
-        self.assertFalse(self.sn.backup(force = True))
+        self.assertFalse(self.sn.backup(force=True))
         self.assertTrue(takeSnapshot.called)
 
-    @patch('time.sleep') # speed up unittest
     @patch.object(ApplicationInstance, 'check')
     @patch('snapshots.Snapshots.takeSnapshot')
     def test_already_running(self, takeSnapshot, check, sleep):
@@ -219,7 +208,6 @@ class TestBackup(generic.SnapshotsTestCase):
         self.assertIs(self.sn.backup(), True)
         self.assertFalse(takeSnapshot.called)
 
-    @patch('time.sleep') # speed up unittest
     @patch.object(PluginManager, 'processBegin')
     @patch('snapshots.Snapshots.takeSnapshot')
     def test_plugin_prevented_backup(self, takeSnapshot, processBegin, sleep):
@@ -228,7 +216,6 @@ class TestBackup(generic.SnapshotsTestCase):
         self.assertIs(self.sn.backup(), True)
         self.assertFalse(takeSnapshot.called)
 
-    @patch('time.sleep') # speed up unittest
     @patch.object(config.Config, 'isConfigured')
     @patch('snapshots.Snapshots.takeSnapshot')
     def test_not_configured(self, takeSnapshot, isConfigured, sleep):
@@ -237,7 +224,6 @@ class TestBackup(generic.SnapshotsTestCase):
         self.assertIs(self.sn.backup(), True)
         self.assertFalse(takeSnapshot.called)
 
-    @patch('time.sleep') # speed up unittest
     @patch.object(Mount, 'mount')
     @patch('snapshots.Snapshots.takeSnapshot')
     def test_mount_exception(self, takeSnapshot, mount, sleep):
@@ -246,7 +232,6 @@ class TestBackup(generic.SnapshotsTestCase):
         self.assertIs(self.sn.backup(), True)
         self.assertFalse(takeSnapshot.called)
 
-    @patch('time.sleep') # speed up unittest
     @patch.object(Mount, 'umount')
     @patch('snapshots.Snapshots.takeSnapshot')
     def test_umount_exception(self, takeSnapshot, umount, sleep):
@@ -254,7 +239,6 @@ class TestBackup(generic.SnapshotsTestCase):
         takeSnapshot.return_value = [True, False]
         self.assertIs(self.sn.backup(), False)
 
-    @patch('time.sleep') # speed up unittest
     @patch.object(config.Config, 'canBackup')
     @patch('snapshots.Snapshots.takeSnapshot')
     def test_cant_backup(self, takeSnapshot, canBackup, sleep):
@@ -263,7 +247,6 @@ class TestBackup(generic.SnapshotsTestCase):
         self.assertIs(self.sn.backup(), True)
         self.assertFalse(takeSnapshot.called)
 
-    @patch('time.sleep') # speed up unittest
     @patch('snapshots.Snapshots.takeSnapshot')
     def test_takeSnapshot_exception_cleanup(self, takeSnapshot, sleep):
         takeSnapshot.side_effect = Exception('Boom')
