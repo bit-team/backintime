bit-team/backintime

commit 8daa19ab57826ab16147f102e6ae5b88b02fc1d7

translate: Minor modifications
Some minor fixes and modifications related to the last update of translation strings. The need of this modifications came up while doing the German translation at Weblate.

common/config.py
@@ -94,7 +94,7 @@ class Config(configfile.ConfigFileWithProfiles):
     SCHEDULE_MODES = {
                 NONE: _('Disabled'),
                 AT_EVERY_BOOT: _('At every boot/reboot'),
-                _5_MIN: _('Every 5 minutes'),
+                _5_MIN: _('Every {n} minutes'),
                 _10_MIN: _('Every {n} minutes').format(n=10),
                 _30_MIN: _('Every {n} minutes').format(n=30),
                 _1_HOUR: _('Every hour'),

common/snapshots.py
@@ -1646,7 +1646,8 @@ restore is done. The pid of the already running restore is in %s.  Maybe delete 
             minFreeInodes = self.config.minFreeInodes()
             self.setTakeSnapshotMessage(
                 0,
-                _('Trying to keep min {perc}% free inodes').format(perc=minFreeInodes)
+                _('Trying to keep min {perc}% free inodes')
+                .format(perc=minFreeInodes)
             )
             logger.debug(
                 "Keep min {perc}% free inodes".format(perc=minFreeInodes),

qt/settingsdialog.py
@@ -2110,19 +2110,18 @@ class RestoreConfigDialog(QDialog):
         except KeyError:
             pass
 
-        label = QLabel(
-            _('Please navigate to the snapshot from which you want '
-              'to restore %(appName)s\'s configuration. The path '
-              'may look like: \n%(samplePath)s\n\n'
-              'If your snapshots are on a remote drive or if they are '
-              'encrypted you need to manually mount them first. '
-              'If you use Mode SSH you also may need to set up public key '
-              'login to the remote host%(addFuse)s.\n'
-              'Take a look at \'man backintime\'.') % {
-                  'appName': self.config.APP_NAME,
-                  'samplePath': samplePath,
-                  'addFuse': addFuse
-              },
+        label = QLabel(_(
+            "Please navigate to the snapshot from which you want to restore "
+            "{appName}'s configuration. The path may look like:\n"
+            "{samplePath}\n\nIf your snapshots are on a remote drive or if "
+            "they are ncrypted you need to manually mount them first. "
+            "If you use Mode SSH you also may need to set up public key "
+            "login to the remote host{addFuse}.\n"
+            "Take a look at 'man backintime'.")
+            .format(
+                appName=self.config.APP_NAME,
+                samplePath=samplePath,
+                addFuse=addFuse),
             self
         )
         label.setWordWrap(True)

common/dummytools.py
@@ -76,7 +76,7 @@ class Dummy(mount.MountControl):
     def preMountCheck(self, first_run = False):
         """
         check what ever conditions must be given for the mount to be donesuccessful
-        raise MountException(_('Error description')) if service can not mount
+        raise MountException('Error description') if service can not mount
         return True if everything is okay
         all pre|post_[u]mount_check can also be used to prepare things or clean up
         """
@@ -85,20 +85,20 @@ class Dummy(mount.MountControl):
     def postMountCheck(self):
         """
         check if mount was successful
-        raise MountException(_('Error description')) if not
+        raise MountException('Error description') if not
         """
         return True
 
     def preUmountCheck(self):
         """
         check if service is safe to umount
-        raise MountException(_('Error description')) if not
+        raise MountException('Error description') if not
         """
         return True
 
     def postUmountCheck(self):
         """
         check if umount successful
-        raise MountException(_('Error description')) if not
+        raise MountException('Error description') if not
         """
         return True

common/encfstools.py
@@ -147,7 +147,7 @@ class EncFS_mount(MountControl):
                 )
 
                 if not self.config.askQuestion(question):
-                    raise MountException(_('Cancel'))
+                    raise MountException('Cancel')
 
                 else:
                     pw = password.Password(self.config)
@@ -156,7 +156,7 @@ class EncFS_mount(MountControl):
                     if self.password == password_confirm:
                         return False
                     else:
-                        raise MountException(_("Password doesn't match"))
+                        raise MountException("Password doesn't match")
 
     def checkVersion(self):
         """

common/mount.py
@@ -703,11 +703,10 @@ class MountControl(object):
         while self.checkLocks(lock_path, lockSuffix):
             count += 1
             if count == timeout:
-                raise MountException(_('Mountprocess lock timeout'))
+                raise MountException('Mountprocess lock timeout')
             sleep(1)
 
-        logger.debug('Acquire mountprocess lock %s'
-                     %lock, self)
+        logger.debug(f'Acquire mountprocess lock {lock}', self)
         with open(lock, 'w') as f:
             f.write(self.pid)
 
@@ -718,8 +717,7 @@ class MountControl(object):
         lock_path = self.mount_root
         lockSuffix = '.lock'
         lock = os.path.join(lock_path, self.pid + lockSuffix)
-        logger.debug('Release mountprocess lock %s'
-                     %lock, self)
+        logger.debug(f'Release mountprocess lock {lock}', self)
         if os.path.exists(lock):
             os.remove(lock)
 

common/sshtools.py
@@ -797,14 +797,15 @@ class SSH(MountControl):
                 if err or proc.returncode:
                     logger.debug('rsync command returned error: %s' % err, self)
 
-                    raise MountException(_(
-                        'Remote host %(host)s doesn\'t support'
-                        ' \'%(command)s\':\n'
-                        '%(err)s\nLook at \'man backintime\' for further '
-                        'instructions') % {
-                            'host': self.host,
-                            'command': cmd,
-                            'err': err})
+                    raise MountException(
+                        "Remote host {host} doesn't support '{command}:\n"
+                        "{err}\n"
+                        "Look at 'man backintime' for further instructions."
+                        .format(
+                            host=self.host,
+                            command=cmd,
+                            err=err)
+                    )
 
         # check cp chmod find and rm
         head = 'tmp1="%s"; tmp2="%s"; ' % (remote_tmp_dir_1, remote_tmp_dir_2)

qt/snapshotsdialog.py
@@ -109,7 +109,7 @@ class SnapshotsDialog(QDialog):
 
         #list different snapshots only
         self.cbOnlyDifferentSnapshots = QCheckBox(
-            _('List only different snapshots'), self)
+            _('Differing snapshots only'), self)
         self.mainLayout.addWidget(self.cbOnlyDifferentSnapshots)
         self.cbOnlyDifferentSnapshots.stateChanged.connect(self.cbOnlyDifferentSnapshotsChanged)
 

commit 89cf0d397142bc085132c84a48ababbca48be39b

fix: "5 minutes" translation and fine tuning strings
Fix minor translation bug introduced with 8daa19a.
Workaround Weblate bug misinterpreting not escaped %.
Additional fine tuning of strings.

common/config.py
@@ -94,7 +94,7 @@ class Config(configfile.ConfigFileWithProfiles):
     SCHEDULE_MODES = {
                 NONE: _('Disabled'),
                 AT_EVERY_BOOT: _('At every boot/reboot'),
-                _5_MIN: _('Every {n} minutes'),
+                _5_MIN: _('Every {n} minutes').format(n=5),
                 _10_MIN: _('Every {n} minutes').format(n=10),
                 _30_MIN: _('Every {n} minutes').format(n=30),
                 _1_HOUR: _('Every hour'),
@@ -163,14 +163,30 @@ class Config(configfile.ConfigFileWithProfiles):
     DEFAULT_REDIRECT_STDOUT_IN_CRON = True
     DEFAULT_REDIRECT_STDERR_IN_CRON = False
 
-    exp = _(' EXPERIMENTAL!')
     SNAPSHOT_MODES = {
-                #mode           : (<mounttools>,            'ComboBox Text',        need_pw|lbl_pw_1,       need_2_pw|lbl_pw_2),
-                'local'         : (None,                    _('Local'),             False,                  False),
-                'ssh'           : (sshtools.SSH,            _('SSH'),               _('SSH private key'),   False),
-                'local_encfs'   : (encfstools.EncFS_mount,  _('Local encrypted'),   _('Encryption'),        False),
-                'ssh_encfs'     : (encfstools.EncFS_SSH,    _('SSH encrypted'),     _('SSH private key'),   _('Encryption'))
-                }
+                # mode: (
+                #     <mounttools>,
+                #     'ComboBox Text',
+                #     need_pw|lbl_pw_1,
+                #     need_2_pw|lbl_pw_2
+                # ),
+                'local': (
+                    None, _('Local'), False, False),
+                'ssh': (
+                    sshtools.SSH, 'SSH', _('SSH private key'), False),
+                'local_encfs': (
+                    encfstools.EncFS_mount,
+                    '{} {}'.format(_('Local'), _('encrypted')),
+                    _('Encryption'),
+                    False
+                ),
+                'ssh_encfs': (
+                    encfstools.EncFS_SSH,
+                    _('SSH encrypted'),
+                    _('SSH private key'),
+                    _('Encryption')
+                )
+    }
 
     SSH_CIPHERS = {
         'default': _('Default'),

common/snapshots.py
@@ -785,7 +785,7 @@ restore is done. The pid of the already running restore is in %s.  Maybe delete 
 
                             if ret_error:
                                 logger.error('Failed to take snapshot !!!', self)
-                                self.setTakeSnapshotMessage(1, _('Failed to take snapshot %s !!!') % sid.displayID)
+                                self.setTakeSnapshotMessage(1, _('Failed to take snapshot {snapshot_id} !!!').format(snapshot_id=sid.displayID))
                                 time.sleep(2)
                             else:
                                 logger.warning("No new snapshot", self)
@@ -1646,8 +1646,8 @@ restore is done. The pid of the already running restore is in %s.  Maybe delete 
             minFreeInodes = self.config.minFreeInodes()
             self.setTakeSnapshotMessage(
                 0,
-                _('Trying to keep min {perc}% free inodes')
-                .format(perc=minFreeInodes)
+                _('Trying to keep min {perc} free inodes')
+                .format(perc=f'{minFreeInodes}%')
             )
             logger.debug(
                 "Keep min {perc}% free inodes".format(perc=minFreeInodes),

qt/qtsystrayicon.py
@@ -96,7 +96,10 @@ class QtSysTrayIcon:
 
         self.openLog = self.contextMenu.addAction(icon.VIEW_LAST_LOG, _('View Last Log'))
         self.openLog.triggered.connect(self.onOpenLog)
-        self.startBIT = self.contextMenu.addAction(icon.BIT_LOGO, _('Start BackInTime'))
+        self.startBIT = self.contextMenu.addAction(
+            icon.BIT_LOGO,
+            _('Start {appname}').format(appname=self.config.APP_NAME)
+        )
         self.startBIT.triggered.connect(self.onStartBIT)
         self.status_icon.setContextMenu(self.contextMenu)
 

qt/settingsdialog.py
@@ -161,7 +161,7 @@ class SettingsDialog(QDialog):
         self.fillCombo(self.comboModes, store_modes)
 
         # encfs security warning
-        self.encfsWarning = QLabel('<b>{}:</b>{}'.format(
+        self.encfsWarning = QLabel('<b>{}:</b> {}'.format(
             _('Warning'),
             _('{app} uses EncFS for encryption. A recent security audit '
               'revealed several possible attack vectors for this. Please '
@@ -500,9 +500,16 @@ class SettingsDialog(QDialog):
         layout = QVBoxLayout(tabWidget)
 
         self.lblSshEncfsExcludeWarning = QLabel(
-            _('<b>Warning:</b> Wildcards (\'foo*\', \'[fF]oo\', \'fo?\') '
-              'will be ignored with mode \'SSH encrypted\'.\nOnly separate '
-              'asterisk are allowed (\'foo/*\', \'foo/**/bar\')'), self)
+            "<b>{}:</b> {}".format(
+                _("Warning"),
+                _(
+                    "Wildcards ('foo*', '[fF]oo', 'fo?') will be ignored "
+                    "with mode 'SSH encrypted'.\nOnly separate asterisk "
+                    "are allowed ('foo/*', 'foo/**/bar')"
+                )
+            ),
+            self
+        )
         self.lblSshEncfsExcludeWarning.setWordWrap(True)
         layout.addWidget(self.lblSshEncfsExcludeWarning)
 
@@ -523,7 +530,7 @@ class SettingsDialog(QDialog):
         layout.addWidget(self.listExclude)
         self.listExcludeCount = 0
 
-        label = QLabel(_('Highly recommended:'), self)
+        label = QLabel(_('Highly recommended') + ':', self)
         qttools.setFontBold(label)
         layout.addWidget(label)
         label = QLabel(', '.join(sorted(self.config.DEFAULT_EXCLUDE)), self)
@@ -645,7 +652,10 @@ class SettingsDialog(QDialog):
         smlayout = QGridLayout(widget)
 
         self.cbSmartRemoveRunRemoteInBackground = QCheckBox(
-            _('Run in background on remote Host.') + _(' EXPERIMENTAL!'),
+            '{} {}!'.format(
+                _('Run in background on remote Host.'),
+                _('EXPERIMENTAL')
+            ),
             self)
         smlayout.addWidget(self.cbSmartRemoveRunRemoteInBackground, 0, 0, 1, 3)
 
@@ -868,7 +878,7 @@ class SettingsDialog(QDialog):
             _('Limit rsync bandwidth usage') + ': ', self)
         hlayout.addWidget(self.cbBwlimit)
         self.spbBwlimit = QSpinBox(self)
-        self.spbBwlimit.setSuffix(_(' KB/sec'))
+        self.spbBwlimit.setSuffix(' ' + _('KB/sec'))
         self.spbBwlimit.setSingleStep(100)
         self.spbBwlimit.setRange(0, 1000000)
         hlayout.addWidget(self.spbBwlimit)
@@ -1453,8 +1463,8 @@ class SettingsDialog(QDialog):
 
             if not os.path.isfile(self.txtSshPrivateKeyFile.text()):
                 self.errorHandler(
-                    _('Private key file "%(file)s" does not exist.')
-                    % {'file': self.txtSshPrivateKeyFile.text()}
+                    _('Private key file "{file}" does not exist.')
+                    .format(file=self.txtSshPrivateKeyFile.text())
                 )
                 self.txtSshPrivateKeyFile.setText('')
 
@@ -1633,18 +1643,18 @@ class SettingsDialog(QDialog):
 
                     return False
 
-                msg = _('The authenticity of host "%(host)s" can\'t be '
-                        'established.\n\n%(keytype)s key fingerprint is:')
-                msg = msg % {'host': self.config.sshHost(),
-                             'keytype': keyType}
+                msg = _("The authenticity of host {host} can't be "
+                        "established.\n\n{keytype} key fingerprint is:") \
+                        .format(host='"{}"'.format(self.config.sshHost()),
+                                keytype=keyType)
                 options = []
                 lblFingerprint = QLabel(fingerprint + '\n')
                 lblFingerprint.setWordWrap(False)
                 lblFingerprint.setFont(QFont('Monospace'))
                 options.append({'widget': lblFingerprint, 'retFunc': None})
                 lblQuestion = QLabel(
-                    _('Please verify this fingerprint! Would you like to '
-                      'add it to your \'known_hosts\' file?')
+                    _("Please verify this fingerprint! Would you like to "
+                      "add it to your 'known_hosts' file?")
                 )
                 options.append({'widget': lblQuestion, 'retFunc': None})
 
@@ -1875,10 +1885,13 @@ class SettingsDialog(QDialog):
             if os.path.islink(path) \
                 and not (self.cbCopyUnsafeLinks.isChecked()
                          or self.cbCopyLinks.isChecked()):
-                if self.questionHandler(
-                    _('"%s" is a symlink. The linked target will not be '
-                      'backed up until you include it, too.\nWould you like '
-                      'to include the symlinks target instead?') % path):
+
+                question_msg = _(
+                    '"{path}" is a symlink. The linked target will not be '
+                    'backed up until you include it, too.\nWould you like '
+                    'to include the symlinks target instead?') \
+                    .format(path=path)
+                if self.questionHandler(question_msg):
                     path = os.path.realpath(path)
 
             path = self.config.preparePath(path)