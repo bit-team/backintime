35fabc714ccbad9f026122684e4360d0fe891095
Christian Buhtz <c.buhtz@posteo.jp>
2024-01-14 16:52:10 +0100

    sort order qt6 error

--- qt/app.py
+++ qt/app.py
@@ -1071,8 +1071,9 @@ class MainWindow(QMainWindow):
         self.addPlace(_('Root'), '/', 'computer')
         self.addPlace(_('Home'), os.path.expanduser('~'), 'user-home')
 
-        #add backup folders
+        # add backup folders
         include_folders = self.config.include()
+
         if include_folders:
             folders = []
             for item in include_folders:
@@ -1081,9 +1082,13 @@ class MainWindow(QMainWindow):
 
             if folders:
                 sortColumn = self.places.header().sortIndicatorSection()
-                sortOrder  = self.places.header().sortIndicatorOrder()
+                sortOrder = self.places.header().sortIndicatorOrder()
+
                 if not sortColumn:
-                    folders.sort(key = lambda v: (v.upper(), v[0].islower()), reverse = sortOrder)
+                    folders.sort(
+                        key=lambda v: (v.upper(), v[0].islower()),
+                        reverse=sortOrder == Qt.SortOrder.DescendingOrder)
+
                 self.addPlace(_('Backup folders'), '', '')
                 for folder in folders:
                     self.addPlace(folder, folder, 'document-save')
