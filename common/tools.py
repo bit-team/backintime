2c77ab4a371b2b3d3df38838a9892f23c22d921c
Christian Buhtz <c.buhtz@posteo.jp>
2024-02-08 17:14:57 +0100

    move diagnostics.get_git_repository_info() to tools.py

--- common/tools.py
+++ common/tools.py
@@ -1,20 +1,22 @@
-#    Back In Time
-#    Copyright (C) 2008-2022 Oprea Dan, Bart de Koning, Richard Bailey,
-#    Germar Reitze, Taylor Raack
+"""Collection of helper functions not fitting to other modules.
+"""
+# Back In Time
+# Copyright (C) 2008-2022 Oprea Dan, Bart de Koning, Richard Bailey,
+# Germar Reitze, Taylor Raack
 #
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License along
-#    with this program; if not, write to the Free Software Foundation, Inc.,
-#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 import os
 import sys
 import pathlib
@@ -86,7 +88,6 @@ DISK_BY_UUID = '/dev/disk/by-uuid'
 # |-----------------|
 # | Handling paths  |
 # |-----------------|
-
 
 def sharePath():
     """Get path where Back In Time is installed.
@@ -344,6 +345,50 @@ def addSourceToPathEnviron():
     path = os.getenv('PATH')
     if path and source not in path.split(':'):
         os.environ['PATH'] = '%s:%s' %(source, path)
+
+def get_git_repository_info(path=None):
+    """Return the current branch and last commit hash.
+
+    Credits: https://stackoverflow.com/a/51224861/4865723
+
+    Args:
+        path (Path): Path with '.git' folder in (default is
+                     current working directory).
+
+    Returns:
+        (dict): Dict with keys "branch" and "hash" if it is a git repo,
+                otherwise an `None`.
+    """
+
+    if not path:
+        path = Path.cwd()
+
+    git_folder = path / '.git'
+
+    if not git_folder.exists():
+        return None
+
+    result = {}
+
+    # branch name
+    with (git_folder / 'HEAD').open('r') as handle:
+        val = handle.read()
+
+    if val.startswith('ref: '):
+        result['branch'] = '/'.join(val.split('/')[2:]).strip()
+
+    else:
+        result['branch'] = '(detached HEAD)'
+        result['hash'] = val
+
+        return result
+
+    # commit hash
+    with (git_folder / 'refs' / 'heads' / result['branch']) \
+         .open('r') as handle:
+        result['hash'] = handle.read().strip()
+
+    return result
 
 def gitRevisionAndHash():
     """
@@ -1950,7 +1995,6 @@ class UniquenessSet:
             return False
         else:
             return self.reference == (st.st_size, int(st.st_mtime))
-
 
 class Alarm(object):
     """
