2676134e334c730b4c56624c10569f1ad05ccbcd
Christian Buhtz <c.buhtz@posteo.jp>
2024-02-09 11:47:55 +0100

    module getter

--- common/version.py
+++ common/version.py
@@ -8,14 +8,8 @@ import tools
 __version_base__ = '1.4.4-dev'
 
 
-def __getattr__(attr_name):
-    """Attribute getter for this module providing access to on-demand created
-    variable
-    """
+def _create_full_version_string():
+    return __version_base__
 
-    # Regular version string used by the application.
-    if attr_name == '__version__':
-        return __version_base
 
-    # Ask module for all other attributes
-    return getattr(__name__, attr_name)
+__version__ = _create_full_version_string()
