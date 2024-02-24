bit-team/backintime

commit afebdf4ed267b3cb015f9b18d86cfde3012feae6

refactor: Separate About dialog & improved path handling in "config.py"
Separate the about dialog code into its own file.
I removed some path handling code from config.py and moved some into tools.py.

common/tools.py
@@ -92,10 +92,11 @@ DISK_BY_UUID = '/dev/disk/by-uuid'
 def sharePath():
     """Get path where Back In Time is installed.
 
+    This is similar to $XDG_DATA_DIRS (XDG Base Directory Specification).
     If running from source return default ``/usr/share``.
 
     Returns:
-        str:    share path like::
+        str: share path like::
 
                     /usr/share
                     /usr/local/share
@@ -109,6 +110,45 @@ def sharePath():
         return share
 
     return '/usr/share'
+
+def backintimePath(*path):
+    """
+    Get path inside 'backintime' install folder.
+
+    Args:
+        *path (str):    paths that should be joined to 'backintime'
+
+    Returns:
+        str:            'backintime' child path like::
+
+                            /usr/share/backintime/common
+                            /usr/share/backintime/qt
+    """
+    return os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, *path))
+
+
+def docPath():
+    """Not sure what this path is about.
+    """
+    path = pathlib.Path(sharePath()) / 'doc' / 'backintime-common'
+
+    # Dev note (buhtz, aryoda, 2024-02):
+    # This piece of code originally resisted in Config.__init__() and was
+    # introduced by Dan in 2008. The reason for the existence of this "if"
+    # is unclear.
+
+    # Makefile (in common) does only install into share/doc/backintime-common
+    # but never into the the backintime "binary" path so I guess the if is
+    # a) either a distro-specific exception for a distro package that
+    # (manually?) installs the LICENSE into another path
+    # b) or a left-over from old code where the LICENSE was installed
+    # differently...
+
+    license_file = pathlib.Path(backintimePath()) / 'LICENSE'
+    if license_file.exists():
+        path = backintimePath()
+
+    return str(path)
 
 
 # |---------------------------------------------------|
@@ -296,22 +336,6 @@ def get_native_language_and_completeness(language_code):
 # |------------------------------------|
 # | Miscellaneous, not categorized yet |
 # |------------------------------------|
-
-def backintimePath(*path):
-    """
-    Get path inside 'backintime' install folder.
-
-    Args:
-        *path (str):    paths that should be joined to 'backintime'
-
-    Returns:
-        str:            'backintime' child path like::
-
-                            /usr/share/backintime/common
-                            /usr/share/backintime/qt
-    """
-    return os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, *path))
-
 def registerBackintimePath(*path):
     """
     Add BackInTime path ``path`` to :py:data:`sys.path` so subsequent imports

qt/aboutdlg.py
@@ -1,0 +1,143 @@
+"""The About dialog."""
+# Back In Time
+# Copyright (C) 2008-2022 Oprea Dan, Bart de Koning, Richard Bailey,
+# Germar Reitze
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+import re
+import pathlib
+from PyQt6.QtWidgets import (QLabel,
+                             QVBoxLayout,
+                             QHBoxLayout,
+                             QDialog,
+                             QDialogButtonBox)
+from PyQt6.QtCore import Qt, QSize
+import tools
+import messagebox
+
+
+class AboutDlg(QDialog):  # pylint: disable=too-few-public-methods
+    """The about dialog accessible from the Help menu in the main window."""
+
+    def __init__(self, parent=None):
+        """Initialize and layout."""
+        super().__init__(parent)
+
+        self.parent = parent
+        self.config = parent.config
+
+        import icon  # pylint: disable=import-outside-toplevel
+
+        self.setWindowTitle(_('About') + ' ' + self.config.APP_NAME)
+        logo = QLabel('Icon')
+        logo.setPixmap(icon.BIT_LOGO.pixmap(QSize(48, 48)))
+
+        name = self._create_name_and_version_label()
+
+        homepage = QLabel(
+            self._to_a_href('https://github.com/bit-team/backintime'))
+        homepage.setTextInteractionFlags(
+            Qt.TextInteractionFlag.LinksAccessibleByMouse)
+        homepage.setOpenExternalLinks(True)
+
+        bit_copyright = QLabel(self.config.COPYRIGHT + '\n')
+
+        vlayout = QVBoxLayout(self)
+        hlayout = QHBoxLayout()
+        hlayout.addWidget(logo)
+        hlayout.addWidget(name)
+        hlayout.addStretch()
+        vlayout.addLayout(hlayout)
+        vlayout.addWidget(homepage)
+        vlayout.addWidget(bit_copyright)
+
+        button_box_left = QDialogButtonBox(self)
+        for label, slot in ((_('Authors'), self._msgbox_authors),
+                            (_('Translations'), self._msgbox_translations),
+                            (_('License'), self._msgbox_license)):
+            btn = button_box_left.addButton(
+                label, QDialogButtonBox.ButtonRole.ActionRole)
+            btn.clicked.connect(slot)
+
+        button_box_right = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok)
+        button_box_right.accepted.connect(self.accept)
+
+        hlayout = QHBoxLayout()
+        hlayout.addWidget(button_box_left)
+        hlayout.addWidget(button_box_right)
+        vlayout.addLayout(hlayout)
+
+    def _create_name_and_version_label(self):
+        version = self.config.VERSION
+        ref, hashid = tools.gitRevisionAndHash()
+        git_version = f" git branch '{ref}' hash '{hashid}'" if ref else ''
+
+        name = QLabel(
+            f'<h1>{self.config.APP_NAME} {version}</h1>{git_version}')
+        name.setAlignment(
+            Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+
+        return name
+
+    def _msgbox_authors(self):
+        file_path = pathlib.Path(tools.docPath()) / 'AUTHORS'
+        content = self._read_about_content(file_path)
+
+        return messagebox.showInfo(self, _('Authors'), content)
+
+    def _msgbox_translations(self):
+        file_path = pathlib.Path(tools.docPath()) / 'TRANSLATIONS'
+        content = self._read_about_content(file_path)
+
+        return messagebox.showInfo(self, _('Translations'), content)
+
+    def _msgbox_license(self):
+        file_path = pathlib.Path(tools.docPath()) / 'LICENSE'
+        content = self._read_about_content(file_path)
+
+        return messagebox.showInfo(self, _('License'), content)
+
+    def _read_about_content(self, file_path):
+        content = file_path.read_text('utf-8')
+
+        # Convert URLs and Email into <a href>
+        content = re.sub(r'<(.*?)>', self._to_a_href, content)
+
+        # HTML line breaks
+        content = re.sub(r'\n', '<br>', content)
+
+        return content
+
+    def _to_a_href(self, m):
+        """Create a HTML a-tag out of Website and EMail URIs.
+
+        Args:
+            m (str, re.Match): Match or string to convert.
+
+        Examples:
+            - 'https://foo.bar' becomes
+              '<a href="https://foo.bar">https://foo.bar</a>'
+            - 'foo@bar.com' becomes
+             '<a href="mailto:foo@bar.com">foo@bar.com</a>'
+        """
+        try:
+            raw_string = m.group(1)
+        except AttributeError:
+            raw_string = m
+
+        if '@' in raw_string:
+            return f'<a href="mailto:{raw_string}">{raw_string}</a>'
+
+        return f'<a href="{raw_string}">{raw_string}</a>'

common/backintime.py
@@ -23,7 +23,7 @@ import subprocess
 from datetime import datetime
 from time import sleep
 import json
-
+import pathlib
 import tools
 # Workaround for situations where startApp() is not invoked.
 # E.g. when using --diagnostics and other argparse.Action
@@ -719,8 +719,8 @@ class printLicense(argparse.Action):
         super(printLicense, self).__init__(*args, **kwargs)
 
     def __call__(self, *args, **kwargs):
-        cfg = config.Config()
-        print(cfg.license())
+        license_path = pathlib.Path(tools.docPath()) / 'LICENSE'
+        print(license_path.read_text('utf-8'))
         sys.exit(RETURN_OK)
 
 class printDiagnostics(argparse.Action):

common/config.py
@@ -145,11 +145,6 @@ class Config(configfile.ConfigFileWithProfiles):
         # read yet.
         configfile.ConfigFileWithProfiles.__init__(self, _('Main profile'))
 
-        self._APP_PATH = tools.backintimePath()
-        self._DOC_PATH = os.path.join(tools.sharePath(), 'doc', 'backintime-common')
-        if os.path.exists(os.path.join(self._APP_PATH, 'LICENSE')):
-            self._DOC_PATH = self._APP_PATH
-
         self._GLOBAL_CONFIG_PATH = '/etc/backintime/config'
 
         HOME_FOLDER = os.path.expanduser('~')
@@ -1375,12 +1370,6 @@ class Config(configfile.ConfigFileWithProfiles):
     def setGlobalFlock(self, value):
         self.setBoolValue('global.use_flock', value)
 
-    def appPath(self):
-        return self._APP_PATH
-
-    def docPath(self):
-        return self._DOC_PATH
-
     def appInstanceFile(self):
         return os.path.join(self._LOCAL_DATA_FOLDER, 'app.lock')
 
@@ -1466,23 +1455,6 @@ class Config(configfile.ConfigFileWithProfiles):
 
     def encfsconfigBackupFolder(self, profile_id = None):
         return os.path.join(self._LOCAL_DATA_FOLDER, 'encfsconfig_backup_%s' % self.fileId(profile_id))
-
-    def license(self):
-        return tools.readFile(os.path.join(self.docPath(), 'LICENSE'), '')
-
-    def translations(self):
-        return tools.readFile(os.path.join(self.docPath(), 'TRANSLATIONS'), '')
-
-    def authors(self):
-        return tools.readFile(os.path.join(self.docPath(), 'AUTHORS'), '')
-
-    def changelog(self):
-        for i in ('CHANGES', 'changelog'):
-            f = os.path.join(self.docPath(), i)
-            clog = tools.readFile(f, '')
-            if clog:
-                return clog
-        return ''
 
     def preparePath(self, path):
         if len(path) > 1:

common/diagnostics.py
@@ -211,6 +211,7 @@ def _get_qt_information():
 
     # Themes
     theme_info = {}
+
     if tools.checkXServer():  # TODO use tools.is_Qt_working() when stable
         qapp = PyQt6.QtWidgets.QApplication([])
         theme_info = {

qt/app.py
@@ -21,6 +21,7 @@ import sys
 if not os.getenv('DISPLAY', ''):
     os.putenv('DISPLAY', ':0.0')
 
+import pathlib
 import re
 import subprocess
 import shutil
@@ -100,6 +101,7 @@ import logviewdialog
 from restoredialog import RestoreDialog
 import languagedialog
 import messagebox
+from aboutdlg import AboutDlg
 
 
 class MainWindow(QMainWindow):
@@ -1275,7 +1277,7 @@ class MainWindow(QMainWindow):
 
     def btnAboutClicked(self):
         with self.suspendMouseButtonNavigation():
-            dlg = About(self)
+            dlg = AboutDlg(self)
             dlg.exec()
 
     def btnHelpClicked(self):
@@ -1297,7 +1299,8 @@ class MainWindow(QMainWindow):
         def aHref_lp(m):
             return '<a href="https://bugs.launchpad.net/backintime/+bug/%(id)s">%(txt)s</a>' % {'txt': m.group(0), 'id': m.group(1)}
 
-        msg = self.config.changelog()
+        changelog_path = pathlib.Path(tools.docPath()) / 'CHANGES'
+        msg = changelog_path.read_text('utf-8')
         msg = re.sub(r'https?://[^) \n]*', aHref, msg)
         msg = re.sub(r'(?:LP:|bug) ?#?(\d+)', aHref_lp, msg)
         msg = re.sub(r'\n', '<br>', msg)
@@ -1868,82 +1871,6 @@ files that the receiver requests to be transferred.""")
     def slot_help_translation(self):
         self._open_approach_translator_dialog()
 
-
-class About(QDialog):
-    def __init__(self, parent = None):
-        super(About, self).__init__(parent)
-        self.parent = parent
-        self.config = parent.config
-        import icon
-
-        self.setWindowTitle(_('About') + ' ' + self.config.APP_NAME)
-        logo     = QLabel('Icon')
-        logo.setPixmap(icon.BIT_LOGO.pixmap(QSize(48, 48)))
-        version = backintime.__version__
-
-        gitinfo = tools.get_git_repository_info()
-        if gitinfo:
-            ref = gitinfo['branch']
-            hashid = gitinfo['hash']
-        else:
-            ref, hashid = None, None
-
-        git_version = ''
-        if ref:
-            git_version = " git branch '{}' hash '{}'".format(ref, hashid)
-        name = QLabel('<h1>' + self.config.APP_NAME + ' ' + version + '</h1>' + git_version)
-        name.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
-        homepage = QLabel(self.mkurl('<https://github.com/bit-team/backintime>'))
-        homepage.setTextInteractionFlags(
-            Qt.TextInteractionFlag.LinksAccessibleByMouse)
-        homepage.setOpenExternalLinks(True)
-        bit_copyright = QLabel(self.config.COPYRIGHT + '\n')
-
-        vlayout = QVBoxLayout(self)
-        hlayout = QHBoxLayout()
-        hlayout.addWidget(logo)
-        hlayout.addWidget(name)
-        hlayout.addStretch()
-        vlayout.addLayout(hlayout)
-        vlayout.addWidget(homepage)
-        vlayout.addWidget(bit_copyright)
-
-        buttonBoxLeft  = QDialogButtonBox(self)
-        btn_authors      = buttonBoxLeft.addButton(_('Authors'), QDialogButtonBox.ButtonRole.ActionRole)
-        btn_translations = buttonBoxLeft.addButton(_('Translations'), QDialogButtonBox.ButtonRole.ActionRole)
-        btn_license      = buttonBoxLeft.addButton(_('License'), QDialogButtonBox.ButtonRole.ActionRole)
-
-        buttonBoxRight = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok)
-
-        hlayout = QHBoxLayout()
-        hlayout.addWidget(buttonBoxLeft)
-        hlayout.addWidget(buttonBoxRight)
-        vlayout.addLayout(hlayout)
-
-        btn_authors.clicked.connect(self.authors)
-        btn_translations.clicked.connect(self.translations)
-        btn_license.clicked.connect(self.license)
-        buttonBoxRight.accepted.connect(self.accept)
-
-    def authors(self):
-        return messagebox.showInfo(self, _('Authors'), self.mkurl(self.config.authors()))
-
-    def translations(self):
-        return messagebox.showInfo(self, _('Translations'), self.mkurl(self.config.translations()))
-
-    def license(self):
-        return messagebox.showInfo(self, _('License'), self.config.license())
-
-    def mkurl(self, msg):
-        msg = re.sub(r'<(.*?)>', self.aHref, msg)
-        msg = re.sub(r'\n', '<br>', msg)
-        return msg
-
-    def aHref(self, m):
-        if m.group(1).count('@'):
-            return '<a href="mailto:%(url)s">%(url)s</a>' % {'url': m.group(1)}
-        else:
-            return '<a href="%(url)s">%(url)s</a>' % {'url': m.group(1)}
 
 class ExtraMouseButtonEventFilter(QObject):
     """

commit 5cbffdf5d858231c370149f4cf9b967e0de66011

fix(build): PyLint improve "name-error" detection, activate "import-error"
Principally PyLint setup is improved. Import all BIT modules improve "name-error" detected. Additionally "import-error" is activated.

Sphinx configuration now determine the version dynamically from "backintime.__version__".

Fixed a problem introduced by afebdf4ed2 (refactor AboutDlg and get_git_version_hash) and a merge conflict resolved in the wrong direction.

And other minor modifications.

common/askpass.py
@@ -16,15 +16,11 @@
 
 import os
 import sys
-try:
-    import gtk
-except:
-    pass
-
 import password
 import password_ipc
 import tools
 import config
+
 
 if __name__ == '__main__':
     """
@@ -52,5 +48,6 @@ if __name__ == '__main__':
     #temp mode
     fifo = password_ipc.FIFO(temp_file)
     pw = fifo.read(5)
+
     if pw:
         print(pw)

common/doc-dev/conf.py
@@ -61,7 +61,8 @@ copyright = '2016, Germar Reitze'
 author = 'Germar Reitze'
 
 # Don't edit this variable. It is updated automatically by "updateversion.sh".
-version = '1.4.4-dev'
+import backintime
+version = backintime.__version__
 # The full version, including alpha/beta/rc tags.
 release = version  # '1.3.3-dev'
 

common/qt_probing.py
@@ -15,7 +15,7 @@ logger.openlog()
 #
 # It is called by "tools.is_Qt_working()" normally
 # but you can also execute it manually via
-#     python3 qt5_probing.py
+#     python3 qt_probing.py
 
 # It works by trying to create a QApplication instance
 # Any error indicates that Qt is not available or not correctly configured.
@@ -95,14 +95,15 @@ try:
     logger.debug(f"Current euid: {os.geteuid()}")
     # Jan 25, 2024 Not enabled but just documented here since this "fix" is a hack (assumes hard-coded UID 1000 to be always correct). But it works in 99 % of installations
     # if isRoot():
-    #     logger.debug("Changing euid from root to user as work-around for #1592 (qt5_probing hangs in root cron job)")
+    #     logger.debug("Changing euid from root to user as work-around for #1592 (qt_probing hangs in root cron job)")
     #     # Fix inspired by
     #     # https://stackoverflow.com/questions/71425861/connecting-to-user-dbus-as-root
     #     os.seteuid(1000)
     #     logger.debug(f"New euid: {os.geteuid()}")
 
-    from PyQt6 import QtCore
-    from PyQt6.QtWidgets import QApplication
+    # Disable pylint "import-error" because of TravisCI ppc64le architecture
+    from PyQt6 import QtCore  # pylint: disable=import-error
+    from PyQt6.QtWidgets import QApplication  # pylint: disable=import-error
 
     app = QApplication([''])
 
@@ -117,7 +118,7 @@ try:
     # ("GUI") is active at all (e.g. in headless installations it isn't).
     # See: https://forum.qt.io/topic/3852/issystemtrayavailable-always-crashes-segfault-on-ubuntu-10-10-desktop/6
 
-    from PyQt6.QtWidgets import QSystemTrayIcon
+    from PyQt6.QtWidgets import QSystemTrayIcon # pylint: disable=import-error
     is_sys_tray_available = QSystemTrayIcon.isSystemTrayAvailable()
 
     if is_sys_tray_available:

common/tools.py
@@ -53,9 +53,9 @@ try:
     #       because the latter is still not available here in the global
     #       module code.
     if os.getenv('BIT_USE_KEYRING', 'true') == 'true' and os.geteuid() != 0:
-        import keyring
-        from keyring import backend
-        import keyring.util.platform_
+        import keyring  # pylint: disable=import-error
+        from keyring import backend  # pylint: disable=import-error
+        import keyring.util.platform_  # pylint: disable=import-error
         is_keyring_available = True
 except Exception as e:
     is_keyring_available = False
@@ -389,6 +389,7 @@ def get_git_repository_info(path=None, hash_length=None):
     """
 
     if not path:
+        # Default is current working dir
         path = pathlib.Path.cwd()
     elif isinstance(path, str):
         # WORKAROUND until cmoplete migration to pathlib
@@ -762,7 +763,7 @@ def is_Qt_working(systray_required=False):
     # don't want to crash BiT if this happens...
 
     try:
-        path = os.path.join(backintimePath("common"), "qt5_probing.py")
+        path = os.path.join(backintimePath("common"), "qt_probing.py")
         cmd = [sys.executable, path]
         if logger.DEBUG:
             cmd.append('--debug')
@@ -773,7 +774,7 @@ def is_Qt_working(systray_required=False):
                               universal_newlines=True) as proc:
 
             std_output, error_output = proc.communicate(timeout=30)  # to get the exit code
-            # "timeout" fixes #1592 (qt5_probing.py may hang as root): Kill after timeout
+            # "timeout" fixes #1592 (qt_probing.py may hang as root): Kill after timeout
 
             logger.debug(f"Qt probing result: exit code {proc.returncode}")
 
@@ -787,7 +788,7 @@ def is_Qt_working(systray_required=False):
         logger.error(f"Qt probing script not found: {cmd[0]}")
         raise
 
-    # Fix for #1592 (qt5_probing.py may hang as root): Kill after timeout
+    # Fix for #1592 (qt_probing.py may hang as root): Kill after timeout
     except subprocess.TimeoutExpired:
         proc.kill()
         outs, errs = proc.communicate()

qt/aboutdlg.py
@@ -26,6 +26,7 @@ from PyQt6.QtWidgets import (QLabel,
 from PyQt6.QtCore import Qt, QSize
 import tools
 import messagebox
+import backintime
 
 
 class AboutDlg(QDialog):  # pylint: disable=too-few-public-methods
@@ -80,9 +81,16 @@ class AboutDlg(QDialog):  # pylint: disable=too-few-public-methods
         vlayout.addLayout(hlayout)
 
     def _create_name_and_version_label(self):
-        version = self.config.VERSION
-        ref, hashid = tools.gitRevisionAndHash()
-        git_version = f" git branch '{ref}' hash '{hashid}'" if ref else ''
+        version = backintime.__version__
+        info = tools.get_git_repository_info(
+            # should be the repos root folder
+            path=pathlib.Path(__file__).parent.parent,
+            hash_length=8)
+        try:
+            git_version \
+                = f" git branch '{info['branch']}' hash '{info['hash']}'"
+        except TypeError:
+            git_version = ''
 
         name = QLabel(
             f'<h1>{self.config.APP_NAME} {version}</h1>{git_version}')

qt/plugins/systrayiconplugin.py
@@ -72,7 +72,7 @@ class SysTrayIconPlugin(pluginmanager.Plugin):
 
         try:
 
-            if tools.is_Qt6_working(systray_required=True):
+            if tools.is_Qt_working(systray_required=True):
                 logger.debug("System tray is available to show the BiT system tray icon")
                 return True
 

qt/serviceHelper.py
@@ -69,11 +69,13 @@ try:
 except ImportError:
     pwd = None
 
-import dbus
-import dbus.service
-import dbus.mainloop
-import dbus.mainloop.pyqt6
-from dbus.mainloop.pyqt6 import DBusQtMainLoop
+# "dbus-python" not available for ppc64le architecture
+# "dbus.mainloop.pyqt6" not available via PyPi for any architecture
+import dbus  # pylint: disable=import-error
+import dbus.service  # pylint: disable=import-error
+import dbus.mainloop  # pylint: disable=import-error
+import dbus.mainloop.pyqt6  # pylint: disable=import-error
+from dbus.mainloop.pyqt6 import DBusQtMainLoop  # pylint: disable=import-error
 from PyQt6.QtCore import QCoreApplication
 
 UDEV_RULES_PATH = '/etc/udev/rules.d/99-backintime-%s.rules'

updateversion.sh
@@ -47,11 +47,11 @@ MAINTAINER="Germar Reitze <germar.reitze@gmail.com>"
 # MAINTAINER="BIT Team <dan@le-web.org>"
 # MAINTAINER="BIT Team <bit-dev@python.org>"
 
-update_sphinx_config () {
-  echo "Update '$1'"
-  sed -e "s/^\(\s*\)version = '.*'$/\1version = '$VERSION'/" \
-      -i $1
-}
+# update_sphinx_config () {
+#   echo "Update '$1'"
+#   sed -e "s/^\(\s*\)version = '.*'$/\1version = '$VERSION'/" \
+#       -i $1
+# }
 
 update_app_version () {
   echo "Update '$1'"
@@ -92,7 +92,8 @@ update_changelog () {
 
 update_app_version common/version.py
 
-update_sphinx_config common/doc-dev/conf.py
+# Sphinx now ask "backintime" itself about its version
+# update_sphinx_config common/doc-dev/conf.py
 
 update_man_page common/man/C/backintime.1
 

common/test/test_lint.py
@@ -51,6 +51,8 @@ class MirrorMirrorOnTheWall(unittest.TestCase):
         # Pylint base command
         cmd = [
             'pylint',
+            # Make sure BIT modules can be improted (to detect "no-member")
+            '--init-hook=import sys;sys.path.insert(0, "./../qt");',
             # Storing results in a pickle file is unnecessary
             '--persistent=n',
             # autodetec number of parallel jobs
@@ -74,12 +76,11 @@ class MirrorMirrorOnTheWall(unittest.TestCase):
             'E0602',  # undefined-variable
             'E1101',  # no-member
             'W1401',  # anomalous-backslash-in-string (invalid escape sequence)
+            'E0401',  # import-error
         ]
         cmd.append('--enable=' + ','.join(err_codes))
 
         # Add py files
         cmd.extend(self._collect_py_files())
-
-        # print(f'Execute {cmd=}')
 
         subprocess.run(cmd, check=True)

common/test/test_tools.py
@@ -1024,18 +1024,19 @@ class TestToolsExecuteOsSystem(generic.TestCase):
 
 
 class Tools_FakeFS(pyfakefs_ut.TestCase):
-    """Tests using a fake filesystem.
-    """
+    """Tests using a fake filesystem."""
 
     def setUp(self):
         self.setUpPyfakefs(allow_root_user=False)
 
-    def test_git_repo_info(self):
+    def test_git_repo_info_none(self):
+        """Acutally not a git repo"""
 
-        # not a git repo
         self.assertEqual(tools.get_git_repository_info(), None)
 
-        # simulate a git repo
+    def test_git_repo_info(self):
+        """Simulate a git repo"""
+
         path = pathlib.Path('.git')
         path.mkdir()
 

qt/test/test_lint.py
@@ -51,6 +51,8 @@ class MirrorMirrorOnTheWall(unittest.TestCase):
         # Pylint base command
         cmd = [
             'pylint',
+            # Workaround
+            '--init-hook=import sys;sys.path.insert(0, "./../common");',
             # Storing results in a pickle file is unnecessary
             '--persistent=n',
             # autodetec number of parallel jobs
@@ -74,12 +76,11 @@ class MirrorMirrorOnTheWall(unittest.TestCase):
             'E0602',  # undefined-variable
             'E1101',  # no-member
             'W1401',  # anomalous-backslash-in-string (invalid escape sequence)
+            'E0401',  # import-error
         ]
         cmd.append('--enable=' + ','.join(err_codes))
 
         # Add py files
         cmd.extend(self._collect_py_files())
-
-        # print(f'Execute {cmd=}')
 
         subprocess.run(cmd, check=True)