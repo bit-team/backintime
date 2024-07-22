2de8a4bc76b5dbf84fda4f2cf220122e0e4bcece
Christian Buhtz <c.buhtz@posteo.jp>
2024-07-22 21:50:38 +0200

    update po files from source

--- common/languages.py
+++ common/languages.py
@@ -1,4 +1,4 @@
-# Generated at Mon Jul 22 21:46:55 2024 with help of package "babel" and "polib".
+# Generated at Mon Jul 22 21:49:50 2024 with help of package "babel" and "polib".
 # https://babel.pocoo.org
 # https://github.com/python-babel/babel
 
