7b665244f78912839a98269514cb0cd73621c42a
Christian Buhtz <c.buhtz@posteo.jp>
2024-10-20 08:50:09 +0200

    update version

--- common/version.py
+++ common/version.py
@@ -13,7 +13,7 @@ See Issue #1575 for details about that migration.
 import re
 
 # Version string regularyly used by the application and presented to users.
-__version__ = '1.5.3-dev.3e80feee'
+__version__ = '1.5.3-rc1'
 
 
 def is_release_candidate() -> bool:
