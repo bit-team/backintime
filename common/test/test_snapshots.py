94d846e6d1593b990a550eaf75d4fd4b511a70d4
Christian Buhtz <c.buhtz@posteo.jp>
2024-10-24 14:26:29 +0200

    skip reason for test_change_group

--- common/test/test_snapshots.py
+++ common/test/test_snapshots.py
@@ -580,6 +580,8 @@ class RestorePathInfo(generic.SnapshotsTestCase):
         self.assertEqual(s.st_uid, CURRENTUID)
         self.assertEqual(s.st_gid, CURRENTGID)
 
+    @unittest.skipIf(len(GROUPS) < 2,
+                     'Current user must be a mamber of more than one group.')
     def test_change_group(self):
         newGroup = [x for x in GROUPS if x != CURRENTGROUP][0]
         newGID = grp.getgrnam(newGroup).gr_gid
