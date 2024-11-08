6cc18328e58a3a9a0eb935cd62737be0d7551b5a
Christian Buhtz <c.buhtz@posteo.jp>
2024-11-08 14:32:03 +0100

    chore(translation): Regular update
    A regular translation update without modifying any source strings.
    
    Statistics compared to previous update 826f039 three weeks ago:
    
    STATISTICS
            Total completeness: 74% (+3)
            Number of languages (excl. English): 46 (+/-0)
            100-99% complete: 8 languages (+8)
            90-98% complete: 4 languages (+4)
            50-89% complete: 28 languages (-11)
            less than 50% complete: 6 languages (-1)

--- updateversion.sh
+++ updateversion.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: © 2008 Oprea Dan
+# SPDX-FileCopyrightText: © 2012 Germar Reitze
+# SPDX-FileCopyrightText: © 2022 Jürgen Altfeld (aryoda)
+# SPDX-FileCopyrightText: © 2023 Christian Buhtz <c.buhtz@posteo.jp>
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# This file is part of the program "Back In time" which is released under GNU
+# General Public License v2 (GPLv2). See LICENSES directory or go to
+# <https://spdx.org/licenses/GPL-2.0-or-later.html>.
+
 # Updates all version numbers using the VERSION file
 # and creates a new DEBIAN changelog file for this version
 # by extracting the changes of this version from the
