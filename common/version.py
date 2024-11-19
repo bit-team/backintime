a31c1d30abf3bade0486c6e92f214c1bd9315360
Christian Buhtz <c.buhtz@posteo.jp>
2024-11-19 08:34:09 +0100

    install LICENSES folder

--- common/version.py
+++ common/version.py
@@ -13,7 +13,7 @@ See Issue #1575 for details about that migration.
 import re
 
 # Version string regularyly used by the application and presented to users.
-__version__ = '1.6.0-dev.66189870'
+__version__ = '1.6.0-dev.a5c44451'
 
 
 def is_release_candidate() -> bool:
