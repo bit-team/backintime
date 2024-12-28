983d9c5e832f09ca2ed75320720ea8b20799e246
Samuel Moore <samuelallister@outlook.com>
2024-12-28 14:05:40 +0000

    added snapshotsFullPath from config class

--- common/snapshots.py
+++ common/snapshots.py
@@ -829,7 +829,7 @@ class Snapshots:
                                     _("Can't find snapshots directory '{path}'.\n"
                                      "If it is on a removable drive please "
                                      "plug it in.")
-                                     .format(path=self.snapshotsFullPath(profile_id))
+                                     .format(path=self.config.snapshotsFullPath(profile_id))
                                     + '\n'
                                     + gettext.ngettext('Waiting %s second.',
                                                        'Waiting %s seconds.',
