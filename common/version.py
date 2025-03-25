394dee98c0c07e7f27f5a2f93510d7bbc76569a7
Christian Buhtz <c.buhtz@posteo.jp>
2025-03-25 13:57:13 +0100

    changelog mod

--- common/version.py
+++ common/version.py
@@ -13,7 +13,7 @@ See Issue #1575 for details about that migration.
 import re
 
 # Version string regularyly used by the application and presented to users.
-__version__ = '1.6.0-dev.b0467fa3'
+__version__ = '1.6.0-dev.6379ee4a'
 
 
 def is_release_candidate() -> bool:
