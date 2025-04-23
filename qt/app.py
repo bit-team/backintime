3a995120cf16b22b40053484da7798fafe3f2320
Christian Buhtz <c.buhtz@posteo.jp>
2025-04-23 17:57:54 +0200

    [skip ci]

--- qt/app.py
+++ qt/app.py
@@ -1355,6 +1355,7 @@ class MainWindow(QMainWindow):
         possible. On the long run the GUI should not call the CLI but call CLI
         code directly.
         """
+        rc = True
         self.config.PLUGIN_MANAGER.processBegin()
 
         the_mount = mount.Mount(cfg=self.config)
