ca2176e3f94b8c9dbd729ae01d9d65b82987109f
Christian Buhtz <c.buhtz@posteo.jp>
2025-04-24 21:45:34 +0200

    build: Stop using commit hash in man page version string

--- common/version.py
+++ common/version.py
@@ -13,7 +13,7 @@ See Issue #1575 for details about that migration.
 import re
 
 # Version string regularyly used by the application and presented to users.
-__version__ = '1.6.0-dev.b0467fa3'
+__version__ = '1.6.0-dev.0927479b'
 
 
 def is_release_candidate() -> bool:
