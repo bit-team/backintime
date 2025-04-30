6447b42a76ed83ed911f97a44d410b09d5fe9a10
Christian Buhtz <c.buhtz@posteo.jp>
2025-04-30 21:53:23 +0200

    message box [skip ci]

--- qt/messagebox.py
+++ qt/messagebox.py
@@ -6,8 +6,15 @@
 # General Public License v2 (GPLv2). See LICENSES directory or go to
 # <https://spdx.org/licenses/GPL-2.0-or-later.html>.
 from PyQt6.QtCore import QTimer, Qt
-from PyQt6.QtWidgets import QApplication, QMessageBox, QInputDialog, QLineEdit,\
-    QDialog, QVBoxLayout, QLabel, QDialogButtonBox, QScrollArea
+from PyQt6.QtWidgets import (QApplication,
+                             QDialog,
+                             QDialogButtonBox,
+                             QInputDialog,
+                             QLabel,
+                             QLineEdit,
+                             QMessageBox,
+                             QScrollArea,
+                             QVBoxLayout)
 import qttools
 
 
@@ -21,7 +28,7 @@ def askPasswordDialog(parent, title, prompt, language_code, timeout):
     dialog = QInputDialog()
 
     timer = QTimer()
-    if not timeout is None:
+    if timeout is not None:
         timer.timeout.connect(dialog.reject)
         timer.setInterval(timeout * 1000)
         timer.start()
@@ -118,7 +125,7 @@ def warningYesNo(parent, msg):
         defaultButton=QMessageBox.StandardButton.No)
 
 
-def warningYesNoOptions(parent, msg, options = ()):
+def warningYesNoOptions(parent, msg, options=()):
 
     # Create a dialog
     dlg = QDialog(parent)
@@ -135,7 +142,9 @@ def warningYesNoOptions(parent, msg, options = ()):
         layout.addWidget(opt['widget'])
 
     # Button box
-    buttonBox = QDialogButtonBox(QDialogButtonBox.StandardButton.Yes | QDialogButtonBox.StandardButton.No)
+    buttonBox = QDialogButtonBox(
+        QDialogButtonBox.StandardButton.Yes
+        | QDialogButtonBox.StandardButton.No)
     buttonBox.button(QDialogButtonBox.StandardButton.No).setDefault(True)
     layout.addWidget(buttonBox)
     buttonBox.accepted.connect(dlg.accept)
@@ -147,8 +156,7 @@ def warningYesNoOptions(parent, msg, options = ()):
     return (
         ret,
         {
-            opt['id']:opt['retFunc']()
-            for opt in options
+            opt['id']: opt['retFunc']() for opt in options
             if opt['retFunc'] is not None
         }
     )
