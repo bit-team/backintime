5f4fd1e424ba594c8b86c6b6038cb7e8198d3d53
Christian Buhtz <c.buhtz@posteo.jp>
2025-05-03 19:58:22 +0200

    x

---
+++ doc/manpages/build_manpages.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+# SPDX-FileCopyrightText: © 2025 Christian Buhtz <c.buhtz@posteo.jp>
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# This file is part of the program "Back In time" which is released under GNU
+# General Public License v2 (GPLv2). See LICENSES directory or go to
+# <https://spdx.org/licenses/GPL-2.0-or-later.html>.
+
