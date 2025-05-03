80a4c7ed9d512748ad37fe908b9fdf28aaf999af
Christian Buhtz <c.buhtz@posteo.jp>
2025-05-03 23:05:25 +0200

    inject version

--- doc/manpages/build_manpages.sh
+++ doc/manpages/build_manpages.sh
@@ -6,6 +6,9 @@
 # This file is part of the program "Back In time" which is released under GNU
 # General Public License v2 (GPLv2). See LICENSES directory or go to
 # <https://spdx.org/licenses/GPL-2.0-or-later.html>.
+BIT_VERSION=$(cat ../../VERSION)
+echo "Using BIT_VERSION $BIT_VERSION"
+
 adoc_to_manpage () {
     # the .adoc-file
     adocfile="$1"
@@ -13,7 +16,7 @@ adoc_to_manpage () {
     manfile="${adocfile%.adoc}.gz"
 
     echo "Convert $file into $manfile"
-    asciidoctor --backend manpage "$file" --out-file=- | gzip --best > "$manfile"
+    asciidoctor --backend manpage --attribute=version="$BIT_VERSION" "$file" --out-file=- | gzip --best > "$manfile"
 
     # This is how Debian Lintian would validate a man page file
     LC_ALL=C.UTF-8 MANROFFSEQ='' MANWIDTH=80 man --warnings -E UTF-8 -l -Tutf8 -Z "$manfile" > /dev/null
