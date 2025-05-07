91cc3de99443e00d2fae5e2d147ac340bfca16f6
Christian Buhtz <c.buhtz@posteo.jp>
2025-05-07 19:25:24 +0200

    [skip ci]

--- common/snapshots.py
+++ common/snapshots.py
@@ -3228,13 +3228,8 @@ def iterSnapshots(cfg: config.Config, includeNewSnapshot: bool = False):
         # at this point. But previously all Exceptions where caught at this
         # point.  Now catching ValueError's only, is a compromise.
         except ValueError as exc:
-            # Raised byi SID.__init__() in case of invalid date format
+            # Raised by SID.__init__() in case of invalid date format
             logger.warning(f'"{item.name}" is not a snapshot ID. {exc=}')
-
-        # except Exception as e:
-        #     if not isinstance(e, LastSnapshotSymlink):
-        #         logger.debug(
-        #             "'{}' is not a snapshot ID: {}".format(item, str(e)))
 
 
 def listSnapshots(cfg, includeNewSnapshot=False, reverse=True):
