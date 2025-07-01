4bb0bd8bfc2286cd342135275f9d6ce5867ac958
Christian Buhtz <c.buhtz@posteo.jp>
2025-07-01 17:25:22 +0200

    sep code. working

--- qt/app.py
+++ qt/app.py
@@ -102,6 +102,7 @@ from aboutdlg import AboutDlg
 from timeline import TimeLine, SnapshotItem
 from bitwidgets import ProfileCombo
 from shutdowndlg import get_shutdown_confirmation
+from statusbar import StatusBar
 
 
 class MainWindow(QMainWindow):
@@ -265,34 +266,10 @@ class MainWindow(QMainWindow):
         self.contextMenu.addSeparator()
         self.contextMenu.addAction(self.act_show_hidden)
 
-        # ProgressBar
-        layoutWidget = QWidget()
-        layout = QVBoxLayout(layoutWidget)
-        layout.setContentsMargins(0, 0, 0, 0)
-        layoutWidget.setContentsMargins(0, 0, 0, 0)
-        layoutWidget.setLayout(layout)
-        self.progressBar = QProgressBar(self)
-        self.progressBar.setMinimum(0)
-        self.progressBar.setMaximum(100)
-        self.progressBar.setValue(0)
-        self.progressBar.setTextVisible(False)
-        self.progressBar.setContentsMargins(0, 0, 0, 0)
-        self.progressBar.setFixedHeight(5)
-        self.progressBar.setVisible(False)
-
-        self.progressBarDummy = QWidget()
-        self.progressBarDummy.setContentsMargins(0, 0, 0, 0)
-        self.progressBarDummy.setFixedHeight(5)
-
-        self.status = QLabel(self)
-        self.status.setContentsMargins(0, 0, 0, 0)
-
-        layout.addWidget(self.status)
-        layout.addWidget(self.progressBar)
-        layout.addWidget(self.progressBarDummy)
-
-        self.statusBar().addWidget(layoutWidget, 100)
-        self.status.setText(_('Done'))
+        # self.statusBar().addWidget(layoutWidget, 100)
+        self.status_bar = StatusBar(self)
+        self.statusBar().addWidget(self.status_bar, 100)
+        self.status_bar.status.setText(_('Done'))
 
         self.snapshotsList = []
         self.sid = snapshots.RootSnapshot(self.config)
@@ -1172,19 +1149,19 @@ class MainWindow(QMainWindow):
                     self.lastTakeSnapshotMessage[1].replace('\n', ' ')
                 )
 
-            self.status.setText(message)
+            self.status_bar.status.setText(message)
 
         pg = progress.ProgressFile(self.config)
         if pg.fileReadable():
-            self.progressBar.setVisible(True)
-            self.progressBarDummy.setVisible(False)
+            self.status_bar.progressBar.setVisible(True)
+            self.status_bar.progressBarDummy.setVisible(False)
             pg.load()
-            self.progressBar.setValue(pg.intValue('percent'))
+            self.status_bar.progressBar.setValue(pg.intValue('percent'))
             message = ' | '.join(self.getProgressBarFormat(pg, message))
             self.status.setText(message)
         else:
-            self.progressBar.setVisible(False)
-            self.progressBarDummy.setVisible(True)
+            self.status_bar.progressBar.setVisible(False)
+            self.status_bar.progressBarDummy.setVisible(True)
 
         #if not fake_busy:
         #	self.lastTakeSnapshotMessage = None
