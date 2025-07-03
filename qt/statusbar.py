5858e1678171987c1af4acf4758bda41253822f2
Christian Buhtz <c.buhtz@posteo.jp>
2025-07-03 09:28:58 +0200

    [skip ci]

--- qt/statusbar.py
+++ qt/statusbar.py
@@ -9,7 +9,7 @@
 # This file is part of the program "Back In Time" which is released under GNU
 # General Public License v2 (GPLv2). See LICENSES directory or go to
 # <https://spdx.org/licenses/GPL-2.0-or-later.html>.
-"""A modul offering a status bar widget
+"""A module offering a status bar widget
 """
 import os
 from PyQt6.QtWidgets import (QFrame,
