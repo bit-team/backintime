fd30d7d53dfcf967513b7a8869c82d80dca360e6
Christian Buhtz <c.buhtz@posteo.jp>
2025-08-21 11:32:52 +0200

    From source

--- common/languages.py
+++ common/languages.py
@@ -6,7 +6,7 @@
 # General Public License v2 (GPLv2). See LICENSES directory or go to
 # <https://spdx.org/licenses/GPL-2.0-or-later.html>.
 #
-# Generated at Thu Aug 21 11:32:03 2025 with help
+# Generated at Thu Aug 21 11:32:46 2025 with help
 # of package "babel" and "polib".
 # https://babel.pocoo.org
 # https://github.com/python-babel/babel
