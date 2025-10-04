4bdce51a1bb96a538edf4081f5d4e3f8f06d13c1
Christian Buhtz <c.buhtz@posteo.jp>
2025-10-04 14:43:58 +0200

    updateversion.sh

--- common/version.py
+++ common/version.py
@@ -13,7 +13,7 @@ See Issue #1575 for details about that migration.
 import re
 
 # Version string regularyly used by the application and presented to users.
-__version__ = '1.5.5'
+__version__ = '1.5.6'
 
 
 def is_release_candidate() -> bool:
