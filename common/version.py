ddac780d78112949a0fe3fa5ceda10322292a833
Christian Buhtz <c.buhtz@posteo.jp>
2025-10-20 22:24:07 +0200

    [skip ci]

--- common/version.py
+++ common/version.py
@@ -13,7 +13,7 @@ See Issue #1575 for details about that migration.
 import re
 
 # Version string regularyly used by the application and presented to users.
-__version__ = '1.6.0-dev.62c1da15'
+__version__ = '1.6.0-dev.3effc59d'
 
 # Version string ends with lower case ``rc`` and optionally with a number.
 # e.g. "1.6.0rc", "1.6.0-rc", "1.6.0-rc2"
