31970ae06d3ed30b6e836d9c6326bab2c451dc4a
Christian Buhtz <c.buhtz@posteo.jp>
2025-10-25 22:14:49 +0200

    feat: Add application logo
    Add Back In Time's own application logo designed by @gregorydk Gregory Deseck.
    
    - Logo source file is `backintime.Source.svg` containing both full-colored and symbolic versions.
    - PNG versions
      - Several resolutions from 16x16 to 512x512, because not all desktop environments are able to handle SVG files.
      - Helper script `qt/icons/png_from_svg.sh`
        - `rsvg_convert` used for SVG to PNG conversion
        - `optipng` used to optimize PNG file size without losing quality
    - Modified and adapted handling of `show-hidden.svg` icon.
    - Updated Makefile based install routine.
    - Updated desktop shortcut files.
    - CHANGED: New GUI runtime dependency `python3-pyqt6.qtsvg`
    - CHANGED: New build dependency `rsvg_convert`
    - CHANGED: New build dependency `optipng`
    
    Close #1961
    ---
    Co-authored-by: Gregory Deseck <gregorydk@users.noreply.github.com>

--- qt/qttools.py
+++ qt/qttools.py
@@ -27,7 +27,7 @@ import subprocess
 from typing import Union, Iterable, Callable
 from contextlib import contextmanager
 from textdlg import TextDialog
-from PyQt6.QtGui import QDesktopServices, QIcon
+from PyQt6.QtGui import QDesktopServices, QIcon, QPalette
 from PyQt6.QtCore import (QEvent,
                           QLibraryInfo,
                           QLocale,
@@ -52,9 +52,12 @@ import version  # noqa: E402
 import messagebox  # noqa: E402
 
 
+_DARK_MODE_THRESHOLD = 128
+
 # |--------------------------------|
 # | Widget modification & creation |
 # |--------------------------------|
+
 
 def can_render(string, widget):
     """Check if the string can be rendered by the font used by the widget.
@@ -75,6 +78,15 @@ def can_render(string, widget):
             return False
 
     return True
+
+
+def in_dark_mode(widget_or_application: QWidget | QApplication) -> bool:
+    """Determine if the desktop/theme is in dark mode."""
+    palette = widget_or_application.palette()
+
+    window_color = palette.color(QPalette.ColorRole.Window)
+
+    return window_color.value() < _DARK_MODE_THRESHOLD
 
 
 _REX_RICHTEXT = re.compile(
