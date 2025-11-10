02784314e459dbbcc91f2b1527a927b989c8e293
Christian Buhtz <c.buhtz@posteo.jp>
2025-11-10 11:11:02 +0100

    manual integrate de

--- qt/manageprofiles/tab_remove_retention.py
+++ qt/manageprofiles/tab_remove_retention.py
@@ -308,9 +308,9 @@ class RemoveRetentionTab(QDialog):
             _('Run in background on remote host.'), self)
         qttools.set_wrapped_tooltip(
             cb_in_background,
-            (_('The smart remove procedure will run directly on the remote '
+            (_('The retention policy will be executed directly on the remote '
                'machine, not locally. The commands "bash", "screen", and '
-               '"flock" must be installed and available on the '
+               '"flock" must be installed and available on that '
                'remote machine.'),
              _('If selected, Back In Time will first test the '
                'remote machine.')))
