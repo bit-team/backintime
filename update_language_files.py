f4bb8d7c0009f4c8d99179c69f69e11645e575e5
Christian Buhtz <c.buhtz@posteo.jp>
2026-01-26 18:41:25 +0100

    x

--- update_language_files.py
+++ update_language_files.py
@@ -343,7 +343,7 @@ def update_desktop_files():
                 content = content + translations
 
     # remove blank lines
-    content = list(filter(lambda line: len(line)))
+    content = list(filter(lambda line: len(line), content))
 
     # ensure newline at end of file
     content = content + ['\n']
