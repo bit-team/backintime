9ca9c83266e5acae835b8d90f3e6b8d288338d03
Christian Buhtz <c.buhtz@posteo.jp>
2026-01-26 06:57:03 +0100

    x

--- common/languages.py
+++ common/languages.py
@@ -6,7 +6,7 @@
 # General Public License v2 (GPLv2). See LICENSES directory or go to
 # <https://spdx.org/licenses/GPL-2.0-or-later.html>.
 #
-# Generated at Mon Jan 26 06:38:23 2026 with help
+# Generated at Mon Jan 26 06:56:43 2026 with help
 # of package "babel" and "polib".
 # https://babel.pocoo.org
 # https://github.com/python-babel/babel
