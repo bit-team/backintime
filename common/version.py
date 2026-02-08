f4bf7888f8e95cd658f61aad4f40783f1ac6b5bb
Christian Buhtz <c.buhtz@posteo.jp>
2026-02-08 08:58:38 +0100

    chore: Release 1.6.0

--- common/version.py
+++ common/version.py
@@ -13,7 +13,7 @@ See Issue #1575 for details about that migration.
 import re
 
 # Version string regularyly used by the application and presented to users.
-__version__ = '1.6.0-rc2'
+__version__ = '1.6.0'
 
 # Version string ends with lower case ``rc`` and optionally with a number.
 # e.g. "1.6.0rc", "1.6.0-rc", "1.6.0-rc2"
