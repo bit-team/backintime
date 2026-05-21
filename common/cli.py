75e178ee4f849ceeb6301133c6437baa61b4aa9f
Christian Buhtz <c.buhtz@posteo.jp>
2026-05-21 11:08:51 +0200

    x

--- common/cli.py
+++ common/cli.py
@@ -448,7 +448,13 @@ def _backup_and_remove_encfs_config(cfg: config.Config) -> bool:
             first_pid = cfg.addProfile(cfg.default_profile_name)
             logger.critical(f'Reset Hauptprofil. {first_pid=}')
     cfg.removeProfile(temp_pid)
+
     cfg.save()
+
+    # If main profil was reset and it is the only profile left,
+    # delete the whole config file.
+    if '1' is in encfs_pids and len(cfg.profiles()) == 1:
+        config_fp.unlink()
 
     logger.critical(
         f'A backup of the current config file was created: {config_fp} -> '
