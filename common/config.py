2e7c6186707777ba0c8a0f311729324c72101483
Christian Buhtz <c.buhtz@posteo.jp>
2026-06-19 11:35:19 +0200

    x

--- common/config.py
+++ common/config.py
@@ -954,7 +954,6 @@ class Config(configfile.ConfigFileWithProfiles):
 
     def minFreeSpaceAsStorageSize(self, profile_id = None):
         enabled, value, unit = self.minFreeSpace(profile_id)
-        print(f'minFreeSpaceAsStorageSize() :: {enabled=} {value=} {unit=}')  # DEBUG
 
         return (
             enabled,
@@ -1347,15 +1346,12 @@ class Config(configfile.ConfigFileWithProfiles):
         It is assumed as configured if a snapshot path (backup destination) is
         and include files/directories (backup source) are given.
         """
-        # print(f'isConfigured() {profile_id=} {self.currentProfile()=}')
         if not profile_id:
             profile_id = self.profiles()[0]
-            # print(f' first is {profile_id=}')
 
         path = self.snapshotsPath(profile_id)
         includes = self.include(profile_id)
 
-        # print(f'{path=} {includes=}')
         if bool(path and includes):
             return True
 
