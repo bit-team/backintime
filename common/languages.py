2f4039d848fedf5b69b99674e7cabd1dff252fbd
Christian Buhtz <c.buhtz@posteo.jp>
2026-06-28 13:41:46 +0200

    src string fix

--- common/languages.py
+++ common/languages.py
@@ -6,7 +6,7 @@
 # General Public License v2 (GPLv2). See LICENSES directory or go to
 # <https://spdx.org/licenses/GPL-2.0-or-later.html>.
 #
-# Generated at Sun Jun 28 13:25:36 2026 with help
+# Generated at Sun Jun 28 13:41:40 2026 with help
 # of package "babel" and "polib".
 # https://babel.pocoo.org
 # https://github.com/python-babel/babel
